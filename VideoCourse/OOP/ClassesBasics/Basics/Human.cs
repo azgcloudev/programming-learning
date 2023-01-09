4ec4edd1f18c0e146f76fd805485958b63680cb5
Aldair <aldair.zamora@outlook.com>
2023-01-09 11:05:55 -0600

    classes intro

---
+++ VideoCourse/OOP/ClassesBasics/Basics/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Basics
+{
+    // this class is a blueprint for a datatype
+    class Human
+    {
+        // member variable
+        public string firstName = "Michael"; // need public to be added to be access from a different file
+        public string lastName = "Hook";
+
+        // member method
+        public void IntroduceMySelf()
+        {
+            Console.WriteLine("Hi, I'm {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ VideoCourse/OOP/ClassesBasics/Basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an object of my class
+            // instance of Human
+            Human denis = new Human();
+
+            denis.firstName = "Denis";  // access public variable from outside and change it
+            denis.IntroduceMySelf();    // call method of an object
+
+            Human michael = new Human();
+            michael.IntroduceMySelf();
+            michael.firstName = "Mikel";
+            michael.IntroduceMySelf();
+
+            Human aldair = new Human();
+            aldair.firstName = "Aldair";
+            aldair.lastName = "Zamora";
+
+            Human joe = new Human();
+            joe.firstName = "Joe";
+            joe.lastName = "Doe";
+
+            aldair.IntroduceMySelf();
+            joe.IntroduceMySelf();
+
+            Console.ReadKey();
+        }
+    }
+}
