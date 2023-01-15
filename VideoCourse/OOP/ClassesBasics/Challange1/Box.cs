84b6decc470788905df3e6bb4f57e371cc688e6c
Aldair <aldair.zamora@outlook.com>
2023-01-14 23:17:15 -0600

    study: challange1, classes members and finalizers

---
+++ VideoCourse/OOP/ClassesBasics/Challange1/Box.cs
@@ -0,0 +1,25 @@
+﻿namespace Challange1
+{
+    class Box
+    {
+        // create a read only property "FrontSurface" which calculates the
+        // front surface based on height and length
+        private int height;
+        private int length;
+
+        public Box(int length, int height)
+        {
+            this.length = length;
+            this.height = height;
+        }
+
+
+        // methods to get frontsurface
+        public int FrontSurface
+        {
+            get { return height * length; }
+
+        }
+
+    }
+}
---
+++ VideoCourse/OOP/ClassesBasics/Members/Members.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Members
+{
+    class Members
+    {
+        // member - private field
+        private string memberName;
+        private string jobTitle;
+        private int salary = 2000;
+
+        // member - public field
+        public int age;
+
+        // member - property -  exposes jobTitle safely
+        // properties start with capital letter
+        public string JobTitle
+        {
+            get
+            {
+                return jobTitle;
+            }
+            set
+            {
+                jobTitle = value;
+            }
+        }
+
+
+        // public member Method - can be called from other classes
+        public void Introducing(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine("Hi my name is {0}, and my job title is {1}. I'm {2} years old", memberName, jobTitle, age);
+            }
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine("My salary is {0}", salary);
+        }
+
+
+        // member constructor
+        public Members()
+        {
+            age = 30;
+            memberName = "Juan";
+            salary = 60000;
+            jobTitle = "Senior Program Manager";
+            Console.WriteLine("Object created");
+        }
+
+
+
+        // member - finalizer - destructor
+        // can be created one per class
+        // cannot be inheritent
+        ~Members()
+        {
+            // clean statements
+            Console.WriteLine("Deconstruction of Members object");
+        }
+
+    }
+}
