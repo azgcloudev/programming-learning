a7373a6b82682b9acd812621a677d51bb2cb1056
Aldair <aldair.zamora@outlook.com>
2023-01-17 10:54:07 -0600

    study day arrays, foreach, develop program1

--- VideoCourse/Collections/Arrays/Program.cs
+++ VideoCourse/Collections/Arrays/Program.cs
@@ -15,6 +15,7 @@ namespace Arrays
             grades[4] = 30;
 
             Console.WriteLine("Grades at index 3: {0}", grades[3]);
+            Console.Write("Enter another grade: ");
             string input = Console.ReadLine();
             grades[3] = int.Parse(input);
             Console.WriteLine("Grades at index 3: {0}", grades[3]);
@@ -26,7 +27,34 @@ namespace Arrays
             int[] mathGradesB = new int[] { 1, 2, 3, 4, 5 };
 
             Console.WriteLine("Length of mathGradesB is {0}", mathGradesB.Length);
-            Console.ReadKey();
+
+
+            // FOR EACH LOOP
+            Console.WriteLine("\n------- FOR EACH ------");
+            int[] nums = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = i + 10;
+            }
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine("Element{0} = {1}", j, nums[j]);
+            }
+
+            Console.WriteLine("\n--- for each looping ---");
+            foreach (int k in nums)
+            {
+                Console.WriteLine("nums = {0}", k);
+            }
+
+
+            string[] friends = new string[] { "Diego", "Randall", "Andres", "Juan", "Les" };
+            Console.WriteLine("\n"); //white space
+            foreach (string name in friends)
+            {
+                Console.WriteLine("Hello {0} welcome! Hope you are good.", name);
+            }
         }
     }
 }
---
+++ VideoCourse/Collections/Program1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        private static void InputedValue(string value)
+        {
+            Console.WriteLine("\nYou have entered the value: {0}", value);
+        }
+
+        private static void InvalidType(string type)
+        {
+            Console.WriteLine("It is an invalid: {0}", type);
+        }
+
+        private static void ValidType(string type)
+        {
+            Console.WriteLine("It is an valid: {0}", type);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a value: ");
+            string valueInput = Console.ReadLine();
+
+            Console.WriteLine("\nSelect the Data Type to validate the input you have entered.");
+            Console.WriteLine("Press 1 for String");
+            Console.WriteLine("Press 2 for Integer");
+            Console.WriteLine("Press 3 for Boolean");
+            Console.Write(": ");
+            int type = int.Parse(Console.ReadLine());
+
+
+
+            if (type == 3)
+            {
+                // check if is boolean
+                if (valueInput.ToLower() == "true" || valueInput.ToLower() == "false")
+                {
+                    InputedValue(valueInput);
+                    ValidType("Boolean");
+                }
+            }
+            else if (type == 2)
+            {
+                bool isInteger = true;
+                foreach (char ch in valueInput)
+                {
+                    if (Char.IsLetter(ch) != true && ch.ToString() != " ")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        isInteger = false;
+                        break;
+                    }
+                }
+
+                if (isInteger)
+                {
+                    InputedValue(valueInput);
+                    ValidType("Integer");
+                }
+                else
+                {
+                    InputedValue(valueInput);
+                    InvalidType("Integer");
+                }
+
+            }
+            else if (type == 1)
+            {
+                bool isString = false;
+
+                foreach (char ch in valueInput)
+                {
+                    if (Char.IsLetter(ch) == true)
+                    {
+                        isString = true;
+                    }
+                }
+
+                if (isString)
+                {
+                    InputedValue(valueInput);
+                    ValidType("String");
+                }
+                else
+                {
+                    InputedValue(valueInput);
+                    InvalidType("String");
+                }
+            }
+
+        }
+    }
+}
