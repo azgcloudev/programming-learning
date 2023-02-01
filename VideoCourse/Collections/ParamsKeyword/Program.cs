7238e359305370c3380d02bd9c6ab7dc7db17d17
Aldair <aldair.zamora@outlook.com>
2023-02-01 10:50:24 -0600

    arrays as parameters; params keyword

---
+++ VideoCourse/Collections/ParamsKeyword/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ParamsKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsMethod("Hello", "String", "Aldair", "Keywords");
+
+            int number = 1;
+            char symbol = '$';
+            string name = "Aldair";
+            float pi = 3.1415F;
+
+            ParamsMethod2(number, symbol, name, pi);
+        }
+
+        // params allows to enter multiple values
+
+        static void ParamsMethod(params string[] words)
+        {
+            //print each string in the parameters
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+
+        static void ParamsMethod2(params object[] obj)
+        {
+            foreach (object str in obj)
+            {
+                // objects has a string method
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
