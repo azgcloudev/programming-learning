7c2579751b147800ba741f6dceb91d819e716ee1
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-02 16:36:58 -0600

    study sql

--- SQLBolt-course.sql
+++ SQLBolt-course.sql
@@ -75,3 +75,25 @@ SELECT * FROM Movies ORDER BY Title ASC LIMIT 5 OFFSET 5;
 
 
 -- ============= SQL Review: Simple SELECT Queries =====================
+-- Table: North_american_cities
+-- City	Country	Population	Latitude	Longitude
+
+-- 1. List all the Canadian cities and their populations
+SELECT City, Population FROM North_american_cities WHERE Country LIKE "canada";
+
+-- 2. Order all the cities in the United States by their latitude from north to south
+SELECT City FROM North_american_cities WHERE Country LIKE "United States" ORDER BY Latitude DESC;
+
+-- 3. List all the cities west of Chicago, ordered from west to east
+SELECT City FROM North_american_cities WHERE Longitude < -87.629798 ORDER BY Longitude ASC;
+
+-- 4. List the two largest cities in Mexico (by population)
+SELECT City FROM North_american_cities WHERE Country LIKE "Mexico" ORDER BY Population DESC LIMIT 2;
+
+-- 5. List the third and fourth largest cities (by population) in the United States and their population
+SELECT City, Population FROM North_american_cities WHERE Country LIKE "united states" ORDER BY Population DESC LIMIT 2 OFFSET 2;
+
+
+
+
+-- ============= SQL Lesson 6: Multi-table queries with JOINs =====================
