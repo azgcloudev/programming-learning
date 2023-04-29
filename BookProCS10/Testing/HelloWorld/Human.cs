53f83abcb5be6eab37bd1621598c6d5732bb4574
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-29 10:13:29 -0600

    update

---
+++ BookProCS10/Testing/HelloWorld/Human.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+	public class Human
+	{
+		private String name;
+
+		public void setName(String name)
+		{
+			this.name = name;
+		}
+
+		public void getName()
+		{
+			Console.WriteLine(name);
+		}
+	}
+}
+
---
+++ BookProCS10/Testing/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿global using System;
+
+// attribute to check if code is CLS compliant
+[assembly: CLSCompliant(true)]
+namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+}
+
