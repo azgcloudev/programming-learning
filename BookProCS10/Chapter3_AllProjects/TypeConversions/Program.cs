cbed7c9948ba4eef2dc41f0846b5bac77f3e7d2f
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-11 11:05:29 -0600

    initial review of widening and  narrowing

---
+++ BookProCS10/Chapter3_AllProjects/TypeConversions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("***** Fun with Conversions *****");
+
+// add two shorts and print the result
+short numb1 = 9, numb2 = 10;
+// since short is shorter than Int, hence compiler winding automatilly short into
+// int , because there won't be data loss
+Console.WriteLine("{0} + {1}: {2}", numb1, numb2, Add(numb1, numb2));
+Console.ReadLine();
+
+
+// narrowing is the oposite of widening
+// convert from bigger to smaller type
+
+// compile error
+numb1 = 30000;
+numb2 = 30000;
+short answer = Add(numb1, numb2);
+Console.WriteLine(answer);
+
+static int Add(int x, int y)
+{
+    return x + y;
+}
