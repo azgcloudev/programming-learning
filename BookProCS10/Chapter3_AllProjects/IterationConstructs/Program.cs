c518d0a48a7ce9e6b3a759880bc09d7c88dacc4a
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-17 10:06:37 -0600

    implicit type using foreach suing linq

--- BookProCS10/Chapter3_AllProjects/IterationConstructs/Program.cs
+++ BookProCS10/Chapter3_AllProjects/IterationConstructs/Program.cs
@@ -2,6 +2,8 @@
 
 ForEachLoopExample();
 
+LinqQueryOverInts();
+
 Console.ReadKey();
 
 
@@ -32,6 +34,22 @@ static void ForEachLoopExample()
     foreach (int i in myInts)
     {
         Console.WriteLine(i);
+    }
+    Console.WriteLine();
+}
+
+// implicit type using foreach
+static void LinqQueryOverInts()
+{
+    int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
+
+    // LINQ query!
+    var subset = from i in numbers where i < 10 select i;
+    Console.WriteLine("Values in subset: ");
+
+    foreach (var i in subset)
+    {
+        Console.Write("{0} ", i);
     }
     Console.WriteLine();
 }
