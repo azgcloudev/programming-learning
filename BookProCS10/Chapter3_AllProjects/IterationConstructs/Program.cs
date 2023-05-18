a53201887e9dc51d1045891b7913b4964a5d8e0b
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-18 08:43:51 -0600

    review iteration with if/else and pattern matching

--- BookProCS10/Chapter3_AllProjects/IterationConstructs/Program.cs
+++ BookProCS10/Chapter3_AllProjects/IterationConstructs/Program.cs
@@ -1,4 +1,4 @@
-﻿ForLoopExample();  ForEachLoopExample();  LinqQueryOverInts();  WhileLoopExample();  DoWhileLoopExample();  Console.ReadKey();    // FOR LOOP static void ForLoopExample() {     // i is only visible within the scope of the for loop     for(int i = 0; i<4;i++)     {         Console.WriteLine("Number is: {0}", i);     }     // 'i' is not visible here     Console.WriteLine(); }   // using FOREACH static void ForEachLoopExample() {     string[] carTypes = { "Ford", "BMW", "Yugo", "Honda" };     foreach (string c in carTypes)     {         Console.WriteLine(c);     }      int[] myInts = { 10, 20, 30, 40 };     foreach (int i in myInts)     {         Console.WriteLine(i);     }     Console.WriteLine(); }  // implicit type using foreach static void LinqQueryOverInts() {     int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };      // LINQ query!     var subset = from i in numbers where i < 10 select i;     Console.WriteLine("Values in subset: ");      foreach (var i in subset)     {         Console.Write("{0} ", i);     }     Console.WriteLine(); }   // USING WHILE LOOP AND DO WHILE static void WhileLoopExample() {     string userIsDone = "";      // Test on a lower-class copy of the string      while(userIsDone.ToLower() != "yes")     {         Console.WriteLine("In the while loop");         Console.Write("Are you done? [yes] [no]: ");         userIsDone = Console.ReadLine();     }     Console.WriteLine(); }  static void DoWhileLoopExample()
+﻿ForLoopExample();  ForEachLoopExample();  LinqQueryOverInts();  //WhileLoopExample();  //DoWhileLoopExample();  IfElseExample();  IfElsePatternMatching();  Console.ReadKey();    // FOR LOOP static void ForLoopExample() {     // i is only visible within the scope of the for loop     for(int i = 0; i<4;i++)     {         Console.WriteLine("Number is: {0}", i);     }     // 'i' is not visible here     Console.WriteLine(); }   // using FOREACH static void ForEachLoopExample() {     string[] carTypes = { "Ford", "BMW", "Yugo", "Honda" };     foreach (string c in carTypes)     {         Console.WriteLine(c);     }      int[] myInts = { 10, 20, 30, 40 };     foreach (int i in myInts)     {         Console.WriteLine(i);     }     Console.WriteLine(); }  // implicit type using foreach static void LinqQueryOverInts() {     int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };      // LINQ query!     var subset = from i in numbers where i < 10 select i;     Console.WriteLine("Values in subset: ");      foreach (var i in subset)     {         Console.Write("{0} ", i);     }     Console.WriteLine(); }   // USING WHILE LOOP AND DO WHILE static void WhileLoopExample() {     string userIsDone = "";      // Test on a lower-class copy of the string      while(userIsDone.ToLower() != "yes")     {         Console.WriteLine("In the while loop");         Console.Write("Are you done? [yes] [no]: ");         userIsDone = Console.ReadLine();     }     Console.WriteLine(); }  static void DoWhileLoopExample()
 {
     string userIsDone = "";
 
@@ -10,4 +10,68 @@
     } while (userIsDone.ToLower() != "yes");
 
     Console.WriteLine();
+}
+
+
+// USING IF/ELSE STATEMENTS
+// only operates boolean expressions
+
+// equality and relational operators
+// == != < > <= >=
+
+// CANNOT TEST A VALUE AGAINTS 0 OR -1
+static void IfElseExample()
+{
+    Console.WriteLine("=> If/Else, with conditionals:");
+
+    string stringData = "Mt textual data";
+    //// this is iligal, given the length return an int, not a bool
+    //if (stringData.Length)
+    //{
+    //    Console.WriteLine("string is greater than 0 characters");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("string is not greater than 0 characters");
+    //}
+
+
+    // legal, as this resolves to either true or false
+    if (stringData.Length > 0)
+    {
+        Console.WriteLine("string is greater than 0 characters");
+    }
+
+    Console.WriteLine();
+}
+
+
+// IF/ELSE PATTERN MATCHING
+static void IfElsePatternMatching()
+{
+    Console.WriteLine("=> If else Pattern Matching:");
+
+    object testItem1 = 123;
+    object testItem2 = "Hello";
+
+    if (testItem1 is String myStringValue1)
+    {
+        Console.WriteLine($"{myStringValue1} is a string");
+    }
+
+    if (testItem1 is int myValue1)
+    {
+        Console.WriteLine($"{myValue1} is an int");
+    }
+
+    if (testItem2 is String myStringValue2)
+    {
+        Console.WriteLine("{0} is a string", myStringValue2);
+    }
+
+    if (testItem2 is int myValue2)
+    { 
+        Console.WriteLine("{0} is an int", myValue2); 
+    }
+
 }
