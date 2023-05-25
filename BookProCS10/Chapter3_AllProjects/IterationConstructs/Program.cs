e2c18538b8aaec79d74c5f07bf64b8838fa1b289
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-25 10:27:21 -0600

    completed chapter 3

--- BookProCS10/Chapter3_AllProjects/IterationConstructs/Program.cs
+++ BookProCS10/Chapter3_AllProjects/IterationConstructs/Program.cs
@@ -1,4 +1,4 @@
-﻿ForLoopExample();  ForEachLoopExample();  LinqQueryOverInts();  //WhileLoopExample();  //DoWhileLoopExample();  IfElseExample();  IfElsePatternMatching();  IfElsePatternMatchingUpdatedInCSharp9();  ExacuteIfElseUsingConditionalOperator();  ConditionalRedExample();  Console.ReadKey();    // FOR LOOP static void ForLoopExample() {     // i is only visible within the scope of the for loop     for(int i = 0; i<4;i++)     {         Console.WriteLine("Number is: {0}", i);     }     // 'i' is not visible here     Console.WriteLine(); }   // using FOREACH static void ForEachLoopExample() {     string[] carTypes = { "Ford", "BMW", "Yugo", "Honda" };     foreach (string c in carTypes)     {         Console.WriteLine(c);     }      int[] myInts = { 10, 20, 30, 40 };     foreach (int i in myInts)     {         Console.WriteLine(i);     }     Console.WriteLine(); }  // implicit type using foreach static void LinqQueryOverInts() {     int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };      // LINQ query!     var subset = from i in numbers where i < 10 select i;     Console.WriteLine("Values in subset: ");      foreach (var i in subset)     {         Console.Write("{0} ", i);     }     Console.WriteLine(); }   // USING WHILE LOOP AND DO WHILE static void WhileLoopExample() {     string userIsDone = "";      // Test on a lower-class copy of the string      while(userIsDone.ToLower() != "yes")     {         Console.WriteLine("In the while loop");         Console.Write("Are you done? [yes] [no]: ");         userIsDone = Console.ReadLine();     }     Console.WriteLine(); }  static void DoWhileLoopExample()
+﻿ForLoopExample();  ForEachLoopExample();  LinqQueryOverInts();  //WhileLoopExample();  //DoWhileLoopExample();  IfElseExample();  IfElsePatternMatching();  IfElsePatternMatchingUpdatedInCSharp9();  ExacuteIfElseUsingConditionalOperator();  ConditionalRedExample();  SwitchExample();  SwitchOnEnumExample();  ExecutePatternMatchingSwitch();  ExecutePatternMatchingSwitchWithWhen();  Console.WriteLine(FromRainbowClassic("Red"));  Console.WriteLine(RockPaperScissors("rock","paper"));   Console.ReadKey();    // FOR LOOP static void ForLoopExample() {     // i is only visible within the scope of the for loop     for(int i = 0; i<4;i++)     {         Console.WriteLine("Number is: {0}", i);     }     // 'i' is not visible here     Console.WriteLine(); }   // using FOREACH static void ForEachLoopExample() {     string[] carTypes = { "Ford", "BMW", "Yugo", "Honda" };     foreach (string c in carTypes)     {         Console.WriteLine(c);     }      int[] myInts = { 10, 20, 30, 40 };     foreach (int i in myInts)     {         Console.WriteLine(i);     }     Console.WriteLine(); }  // implicit type using foreach static void LinqQueryOverInts() {     int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };      // LINQ query!     var subset = from i in numbers where i < 10 select i;     Console.WriteLine("Values in subset: ");      foreach (var i in subset)     {         Console.Write("{0} ", i);     }     Console.WriteLine(); }   // USING WHILE LOOP AND DO WHILE static void WhileLoopExample() {     string userIsDone = "";      // Test on a lower-class copy of the string      while(userIsDone.ToLower() != "yes")     {         Console.WriteLine("In the while loop");         Console.Write("Are you done? [yes] [no]: ");         userIsDone = Console.ReadLine();     }     Console.WriteLine(); }  static void DoWhileLoopExample()
 {
     string userIsDone = "";
 
@@ -156,4 +156,210 @@ static void ConditionalRedExample()
     Console.WriteLine(string.Join(" ", largeArray));
 
     Console.WriteLine();
+}
+
+
+// LOGICAL OPERATORS
+// AND, OR , NOT
+// AND && all evaluations must be true
+// OR || at least one evaluation be true
+// NOT ! if false returns true, if true returns false
+
+
+
+// THE SWITCH STATEMENT
+static void SwitchExample()
+{
+    Console.WriteLine("1 [C#], 2 [VB]");
+    Console.Write("Please pick your language preference: ");
+
+    string langChoice = Console.ReadLine();
+    int n = int.Parse(langChoice);
+
+    switch (n)
+    {
+        case 1:
+            Console.WriteLine("Good choice, C# is a fine language");
+            break;
+        case 2:
+            Console.WriteLine("VB: OOP, multithreading, and more!");
+            break;
+        default:
+            Console.WriteLine("Well...good luck with that!");
+            break;
+    }
+
+    Console.WriteLine();
+}
+
+
+// switch with ENUM
+static void SwitchOnEnumExample()
+{
+    Console.Write("Enter your favorite day of the week: ");
+    DayOfWeek favDay;
+
+    try
+    {
+        favDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Bad input!");
+        return;
+    }
+
+    switch (favDay)
+    {
+        case DayOfWeek.Sunday:
+            Console.WriteLine("Football!!");
+            break;
+        case DayOfWeek.Monday:
+            Console.WriteLine("Another day, another dollar");
+            break;
+        case DayOfWeek.Tuesday:
+            Console.WriteLine("At least it is not Monday");
+            break;
+        case DayOfWeek.Wednesday:
+            Console.WriteLine("A fine day.");
+            break;
+        case DayOfWeek.Thursday:
+            Console.WriteLine("Almost Friday...");
+            break;
+        // cases can be put togetter for a same result action
+        case DayOfWeek.Friday:
+        case DayOfWeek.Saturday:
+            Console.WriteLine("It's weekend!");
+            break;
+    }
+
+    Console.WriteLine();
+}
+
+
+// PATTERN MATCHING IN SWITCH STATEMENT
+static void ExecutePatternMatchingSwitch()
+{
+    Console.WriteLine("1 [Integer (5)], 2 [String (\"Hi\")], 3 [Decimal (2.5)]");
+    Console.Write("Please choose an option: ");
+    string userChoice = Console.ReadLine();
+    object choice;
+    // this is a standard constant pattern switch statement to set up the example
+    switch (userChoice)
+    {
+        case "1":
+            choice = 5;
+            break;
+        case "2":
+            choice = "Hi";
+            break;
+        case "3":
+            choice = 2.5M;
+            break;
+        default:
+            choice = 5;
+            break;
+    }
+
+    // this is the new pattern matching switch statement
+    switch (choice)
+    {
+        case int i:
+            Console.WriteLine("Your choice is an integer {0}.", i);
+            break;
+        case string s:
+            Console.WriteLine("Your choice is a string {0}", s);
+            break;
+        case decimal d:
+            Console.WriteLine("Your choice is a decimal {0}", d);
+            break;
+        default:
+            Console.WriteLine("Your choice is something else");
+            break;
+    }
+    Console.WriteLine();
+}
+
+static void ExecutePatternMatchingSwitchWithWhen()
+{
+    Console.WriteLine("1 [C#], 2 [VB]");
+    Console.Write("Please pick your language preference: ");
+
+    object langChoice = Console.ReadLine();
+    var choice = int.TryParse(langChoice.ToString(), out int c) ? c : langChoice;
+
+    switch (choice)
+    {
+        case int i when i == 2:
+        case string s when s.Equals("VB", StringComparison.OrdinalIgnoreCase):
+            Console.WriteLine("VB: OOP, multithreading, and more!");
+            break;
+        case int i when i == 1:
+        case string s when s.Equals("C#", StringComparison.OrdinalIgnoreCase):
+            Console.WriteLine("Good choice, c# is a fine language.");
+            break;
+        default:
+            Console.WriteLine("Well...good luck with that!");
+            break;
+    }
+    Console.WriteLine();
+}
+
+
+// USING SWITCH EXPRESSIONS (NEW 8.0)
+static string FromRainbowClassic(string colorBand)
+{
+    //switch (colorBand)
+    //{
+    //    case "Red":
+    //        return "#FF0000";
+    //        break;
+    //    case "Orange":
+    //        return "#FF7F00";
+    //        break;
+    //    case "Yellow":
+    //        return "#FFFF00";
+    //        break;
+    //    case "Green":
+    //        return "#00FF00";
+    //        break;
+    //    case "Blue":
+    //        return "#0000FF";
+    //        break;
+    //    case "Indigo":
+    //        return "#3B0082";
+    //        break;
+    //    case "Violet":
+    //        return "#9400D3";
+    //        break;
+    //    default:
+    //        return "#FFFFFF";
+    //        break;
+    //}
+
+    //// can be written also as:
+    return colorBand switch
+    {
+        "Red" => "#FF0000",
+        "Orange" => "#FF7F00",
+        "Yellow" => "#FFFF00",
+        "Green" => "#00FF00",
+        _ => "#FFFFFF" // default
+    };
+}
+
+
+// switch using tuples
+static string RockPaperScissors(string first, string second)
+{
+    return (first, second) switch
+    {
+        ("rock","paper") => "Paper wins.",
+        ("rock","scissors") => "Rock wins.",
+        ("paper","rock") => "Paper wins.",
+        ("paper","scissors") => "Scissors wins.",
+        ("scissors","rock") => "Rock wins.",
+        ("scissors","paper") => "Scissors wins.",
+        (_,_) => "Tie.",
+    };
 }
