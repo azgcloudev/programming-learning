f480270f3dc85ffe34ff433638d4dbf502d0d32d
Aldair Zamora <aldair.zamora@outlook.com>
2023-06-07 10:45:08 -0600

    local functions review

---
+++ BookProCS10/Chapter4_AllProjects/FunWithLocalFunctions/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("AddWrapper: {0}", AddWrapper(4,6));
+
+static int AddWrapper(int x, int y)
+{
+    return Add();
+
+    int Add()
+    {
+        return x + y;
+    }
+}
+
+// in c# 9.0 local functions
+// allow attributes
