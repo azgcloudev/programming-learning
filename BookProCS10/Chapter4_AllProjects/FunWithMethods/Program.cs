99df04d723af9f2ed83c41157fda3b85c5fc2c53
Aldair Zamora <aldair.zamora@outlook.com>
2023-06-22 10:39:27 -0600

    default value types and output parameter behavior

---
+++ BookProCS10/Chapter4_AllProjects/FunWithMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace FunWithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // DEFAULT BEHAVIOR FOR VALUE TYPES
+            // numbers are VALUE TYPES
+
+            Console.WriteLine("***** Fun with Methods *****\n");
+
+            int x = 9, y = 10;
+            Console.WriteLine("Before call: X: {0}, Y: {1}", x, y);
+            Console.WriteLine("Answer is: X: {0}", Add(x, y));
+            Console.WriteLine("After call: X: {0}, Y: {1}\n", x, y);
+
+            // DEFAULT BEHAVIOR FOR REFERENCE TYPES
+            // output modifier
+            Console.WriteLine("-> Output parameters:");
+            // no need to assign initial value to local variables
+            // used as output parameters, provided the first time
+            // you use them is as output arguments.
+            // C# 7 allows for out parameters to be declared in the method call
+            int ans1;
+            AddOut(y, x, out ans1);
+            AddOut(90, 90, out int ans);
+            Console.WriteLine("90 + 90 = {0}\n", ans);
+
+        }
+
+        // value type arguments are passed by value by default
+        static int Add(int x, int y)
+        {
+            int ans = x + y;
+            // caller will not see these changes
+            // as you are modifying a copy of
+            // the original data
+            x = 10000;
+            x = 88888;
+            return ans;
+        }
+
+
+        // ouput parameters
+        static void AddOut(int x, int y, out int ans)
+        {
+            ans = x + y;
+        }
+    }
+}
