d8fe97774a3979dbc5ea973425d434a9cf32459f
Aldair Zamora <aldair.zamora@outlook.com>
2023-07-12 09:37:27 -0600

    method overloading

---
+++ BookProCS10/Chapter4_AllProjects/FunWithMethodOverloading/Program.cs
@@ -0,0 +1,14 @@
+﻿using static FunWithMethodOverloading.AddOperations;
+
+Console.WriteLine("***** Fun with Method Overloading *****");
+
+// Call int version of Add()
+Console.WriteLine(Add(10, 10));
+
+// Call long Add() version
+Console.WriteLine(Add(900_000_000_000, 900_000_000_000));
+
+// Call double Add() version
+Console.WriteLine(Add(4.3,  4.4));
+
+Console.ReadLine();
