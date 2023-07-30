ee8700509839919e9a6e09151b93418ffc666f88
Aldair Zamora <aldair.zamora@outlook.com>
2023-07-30 16:01:17 -0600

    print and string interpolation

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var helloWorld: String = "Hello World"
+
+
+// ***** printing constantans and variables *****
+print(helloWorld)
+
+// print without line break at the end
+print(helloWorld, terminator: "")
+print("Dummy line")
+
+// string interpolation
+print("The value of helloWorld is '\(helloWorld)'")
