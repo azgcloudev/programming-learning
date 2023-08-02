81d810c6d814e011f6af03e9497afbebd6e9ebfe
Aldair Zamora <aldair.zamora@outlook.com>
2023-08-02 09:38:16 -0600

    bitwise review

---
+++ BookProCS10/Chapter4_AllProjects/FunWithBitwiseOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FunWithBitwiseOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Fun with Bitwise Operations =====");
+            Console.WriteLine("6:{2} & 4:{3} = {0} binary: {1}", 6 & 4, Convert.ToString((6 & 4), 2), Convert.ToString(6, 2), Convert.ToString(4, 2));
+            Console.WriteLine("6:{2} | 4:{3} = {0} binary: {1}", 6 | 4, Convert.ToString((6 | 4), 2), Convert.ToString(6, 2), Convert.ToString(4, 2));
+            Console.WriteLine("6:{2} ^ 4:{3} = {0} binary: {1}", 6 ^ 4, Convert.ToString((6 ^ 4), 2), Convert.ToString(6, 2), Convert.ToString(4, 2));
+            Console.WriteLine("6:{2} << 1 = {0} binary: {1}", 6 << 1, Convert.ToString((6 << 1), 2), Convert.ToString(6, 2));
+            Console.WriteLine("6:{2} >> 1 = {0} binary: {1}", 6 >> 1, Convert.ToString((6 >> 1), 2), Convert.ToString(6, 2));
+            Console.WriteLine("~1:{0} = {1}: b{2}", Convert.ToString(1, 2), ~1, Convert.ToString(~1, 2));
+            Console.WriteLine("Int.MaxValue: {0}", Convert.ToString((int.MaxValue), 2));
+
+            // end
+            Console.ReadLine();
+        }
+    }
+}
