8063cfc4cc21c18d82550d05503fb3b3a55a62d5
Aldair Zamora <aldair.zamora@outlook.com>
2023-08-03 06:19:34 -0600

    bitwise enums

--- BookProCS10/Chapter4_AllProjects/FunWithBitwiseOperations/Program.cs
+++ BookProCS10/Chapter4_AllProjects/FunWithBitwiseOperations/Program.cs
@@ -14,9 +14,13 @@
             Console.WriteLine("Int.MaxValue: {0}", Convert.ToString((int.MaxValue), 2));
 
             // Enums with bitwise
+            Console.WriteLine();
             ContactPreference emailAndPhone = ContactPreference.Email | ContactPreference.Phone;
 
-            Console.WriteLine("None? {0}", (emailAndPhone | ContactPreference.None) == ContactPreference.Email);
+            Console.WriteLine("None? {0}", (emailAndPhone | ContactPreference.None) == emailAndPhone);
+            Console.WriteLine("Email? {0}", (emailAndPhone  | ContactPreference.Email) == emailAndPhone);
+            Console.WriteLine("Phone? {0}", (emailAndPhone | ContactPreference.Phone) == emailAndPhone);
+            Console.WriteLine("Text? {0}", (emailAndPhone | ContactPreference.Ponyexpress) == emailAndPhone);
 
             // end
             Console.ReadLine();
