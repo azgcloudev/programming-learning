f3718c5fb5c98649c2373b38f787c2f179629a4f
Aldair Zamora <aldair.zamora@outlook.com>
2023-08-03 10:11:44 -0600

    review structures

---
+++ BookProCS10/Chapter4_AllProjects/FunWithStructures/Program.cs
@@ -0,0 +1,39 @@
+﻿// Create an initial Point
+Point myPoint;
+myPoint.X = 350;
+myPoint.Y = 730;
+myPoint.Display();
+myPoint.Decrement();
+myPoint.Display();
+
+
+
+// end
+Console.ReadLine();
+
+struct Point
+{
+    // fields
+    public int X;
+    public int Y;
+
+    // methods
+    public void Increment()
+    {
+        X++;
+        Y++;
+    }
+
+    public void Decrement()
+    {
+        X--;
+        Y--;
+    }
+
+
+    // displays the current position
+    public void Display()
+    {
+        Console.WriteLine("X = {0}, Y = {1}", X, Y);
+    }
+}
