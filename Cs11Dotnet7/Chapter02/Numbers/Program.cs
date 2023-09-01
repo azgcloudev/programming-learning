c0113c296b3e3c86a2c6a1e75428f4ce7c6b9081
Aldair Zamora <aldair.zamora@outlook.com>
2023-09-01 10:28:44 -0600

    create numbers project, review decimal and hexadecimal notation

---
+++ Cs11Dotnet7/Chapter02/Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // unsigned integer 0 or positive
+            uint naturalNumber = 23;
+
+            // integer
+            int integerNumber = -23;
+
+            // float, fractional number
+            float realNumber = 2.3F;
+
+            // double precision (fractional default type)
+            double anotherRealNumber = 2.3;
+
+
+            // digit number separator (+ c#7.0)
+            int decimalNotation = 2_000_000; // output will be 1000000
+
+            // binary and hexadecimal notation
+            Console.WriteLine("--- Binary and hexadecimal notation");
+            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+            int hexadecimalNotation = 0x_001E_8480;
+            //all numbers are the same
+            Console.WriteLine($"{decimalNotation == binaryNotation}");
+            Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+        }
+    }
+}
