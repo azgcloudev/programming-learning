0f7cf40122c9d7c4297c7964b25216ddfe315173
Aldair Zamora <aldair.zamora@outlook.com>
2023-09-08 10:31:13 -0600

    start reviewing formatting strings

---
+++ Cs11Dotnet7/Chapter02/Formatting/Program.cs
@@ -0,0 +1,13 @@
+﻿// -- formatting with positional arguments ---
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+Console.WriteLine("{0} apples cost {1:C}", arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+// --- get input from user (console) ---
+Console.Write("Type your first name and press ENTER: ");
+string? firstName = Console.ReadLine();
+Console.Write("Type your age and press ENTER: ");
+string age = Console.ReadLine()!;
+Console.WriteLine($"Hello {firstName}, you look good for {age}.");
