7529dd619aabfad74dc2578bbdb8be1f51955930
Aldair Zamora <aldair.zamora@outlook.com>
2023-09-13 10:23:24 -0600

    start new project review asyncconsole

---
+++ Cs11Dotnet7/Chapter02/AsyncConsole/Program.cs
@@ -0,0 +1,4 @@
+﻿HttpClient client = new HttpClient();
+
+HttpResponseMessage response = await client.GetAsync("https://apple.com");
+WriteLine("Apple home page has {0:N0} bytes", response.Content.Headers.ContentLength);
