fc0e5ce5dcde96c1757d9c807a6e2611e1c0b8eb
Aldair Zamora <aldair.zamora@outlook.com>
2023-09-24 23:50:40 -0600

    strings and variable declarations

---
+++ section3/cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// defining a variable
+	// var card string = "Ace of Spades"
+	// var card = "Ace of Spades"
+	card := "Ace of Spades" // only use this for a new declaration
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
