dc8cab32520fc820539bfbd4b646eb7457657e53
Aldair Zamora <aldair.zamora@outlook.com>
2023-09-25 09:55:31 -0600

    study

--- section3/cards/main.go
+++ section3/cards/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// defining a variable
-	// var card string = "Ace of Spades"
-	// var card = "Ace of Spades"
-	card := "Ace of Spades" // only use this for a new declaration
-	card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+
+	fmt.Println(printState())
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
