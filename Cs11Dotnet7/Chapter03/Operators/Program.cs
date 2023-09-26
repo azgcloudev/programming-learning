c2322df1c02a064e6094e8a862b1d27fb3ea1bbd
Aldair Zamora <aldair.zamora@outlook.com>
2023-09-26 10:22:38 -0600

    review operators

---
+++ Cs11Dotnet7/Chapter03/Operators/Program.cs
@@ -0,0 +1,13 @@
+﻿// postfix operator
+WriteLine("--- postfix operator ---");
+int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");
+
+WriteLine();
+
+// prefix operator
+WriteLine("--- prefix operator ---");
+int c = 3;
+int d = ++c;
+WriteLine($"c is {c}, d is {d}");
