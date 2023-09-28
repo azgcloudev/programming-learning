0a5cefebde90edc8b9508dadaa7b7a250bce82f3
Aldair Zamora <aldair.zamora@outlook.com>
2023-09-28 12:15:44 -0600

    review if statement, pattern matching, switch statement

--- Cs11Dotnet7/Chapter03/SelectionStatements/Program.cs
+++ Cs11Dotnet7/Chapter03/SelectionStatements/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿// if statement
+WriteLine("--- If statement ---");
+string password = "ninja";
+
+if (password.Length < 8)
+{
+    WriteLine("Your password is too short. Use at least 8 characters.");
+}
+else
+{
+    WriteLine("Your password is strong.");
+}
+
+WriteLine();
+
+// --- pattern matching statement ---
+WriteLine("--- Pattern matching statement");
+object o = "3";
+int j = 4;
+
+if (o is int i)
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine($"o is not an int, so it cannot be multiplied.");
+}
+
+WriteLine();
+
+// switch statement
+WriteLine("--- Switch statement ---");
+int number = Random.Shared.Next(1, 7);
+WriteLine($"My random number is {number}.");
+
+switch (number)
+{
+    case 1:
+        WriteLine("One");
+        break;
+    case 2:
+        WriteLine("Two");
+        goto case 1;
+    case 3:
+    case 4:
+        WriteLine("Three or Four");
+        goto case 1;
+    case 5:
+        goto A_label;
+    default:
+        WriteLine("Default");
+        break;
+}
+WriteLine("End of switch statement.");
+A_label:
+WriteLine($"After A_label");
