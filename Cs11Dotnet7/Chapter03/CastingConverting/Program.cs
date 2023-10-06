76c004391d7ba0fc05230212cd931d2997d44bb0
Aldair Zamora <aldair.zamora@outlook.com>
2023-10-06 10:39:59 -0600

    casting numbers

---
+++ Cs11Dotnet7/Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Convert;
+
+// implicit casting (automatically)
+int a = 10;
+double b = a;
+WriteLine($"int {a} assign to double b is {b}.");
+
+// explicit casting (manually)
+double c = 9.8;
+// int d = c; // Program.cs(8,9,8,10): error CS0266: Cannot implicitly convert type 'double' to 'int'. An explicit conversion exists (are you missing a cast?)
+//casting
+int d = (int)c; // decimal points will be lost
+
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+// converting values
+double g = 9.8;
+int h = ToInt32(g);
+WriteLine($"g is {g} and h is {h}.");
