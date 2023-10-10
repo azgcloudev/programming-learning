0cac265b8d7e114f675abf3e48d8e1dc0fb2418f
Aldair Zamora <aldair.zamora@outlook.com>
2023-10-10 11:26:29 -0600

    review embedded structs

--- section2/structs/main.go
+++ section2/structs/main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 // defining a struct
 type person struct {
 	firstName string
 	lastName  string
+	// embedded struct
+	contact contactInfo
 }
 
 func main() {
@@ -13,11 +20,22 @@ func main() {
 	// alex := person{"Alex", "Anderson"}
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 
-	// zero value initialization
-	var alex person
+	// // zero value initialization
+	// var alex person
 
-	fmt.Printf("%+v\n", alex)
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Printf("%+v\n", alex)
+	// fmt.Printf("%+v\n", alex)
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+	// fmt.Printf("%+v\n", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%v\n", jim.contact.email)
 }
