f60d912a0fb573c6eadabd06b93df9a2e6c8b7fe
Aldair Zamora <aldair.zamora@outlook.com>
2023-10-12 10:59:46 -0600

    review how to check overflow in try catch

---
+++ Cs11Dotnet7/Chapter03/CheckingForOverflow/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        WriteLine("Initial value: {0}", x);
+        x++;
+        WriteLine("After incrementing {0}", x);
+        x++;
+        WriteLine("After incrementing {0}", x);
+        x++;
+        WriteLine("After incrementing {0}", x);
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("Code overflowed.");
+}
