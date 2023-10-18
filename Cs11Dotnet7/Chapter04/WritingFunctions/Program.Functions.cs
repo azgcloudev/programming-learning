1e646589d47d1dc228f83cda05c2c5a97d35adaf
Aldair Zamora <aldair.zamora@outlook.com>
2023-10-17 20:07:45 -0600

    start reviewing functions

---
+++ Cs11Dotnet7/Chapter04/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,52 @@
+﻿
+partial class Program
+{
+    static void TimesTable(byte number, byte size = 12)
+    {
+        WriteLine("This is the {0} times table with {1} rows:", number, size);
+        for (int row = 1; row <= size; row++)
+        {
+            WriteLine("{0} x {1} = {2}", row, number, row * number);
+        }
+        WriteLine();
+    }
+
+    static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+    {
+        decimal rate = 0.0M;
+
+        switch (twoLetterRegionCode)
+        {
+            case "CH":
+                rate = 0.08M;
+                break;
+            case "DK":
+            case "NO":
+                rate = 0.25M;
+                break;
+            case "GB":
+            case "FR":
+                rate = 0.2M;
+                break;
+            case "OR":
+            case "AK":
+            case "MT":
+                rate = 0.0M;
+                break;
+            case "ND": // North Dakota
+            case "WI": // Wisconsin
+            case "ME": // Maine
+            case "VA": // Virginia
+                rate = 0.05M;
+                break;
+            case "CA": // California
+                rate = 0.0825M;
+                break;
+            default: // most US states
+                rate = 0.06M;
+                break;
+        }
+        return amount * rate;
+
+    }
+}
---
+++ Cs11Dotnet7/Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,3 @@
+﻿//TimesTable(2, size: 20);
+decimal taxToPay = CalculateTax(149, "FR");
+WriteLine("You must pay {0} in tax.", taxToPay);
