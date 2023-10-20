494ba2dbcd0d92a90eafc83f1d20c684bffce10b
Aldair Zamora <aldair.zamora@outlook.com>
2023-10-20 08:25:04 -0600

    remiewed recursion functions and check overflow of number types

--- Cs11Dotnet7/Chapter04/WritingFunctions/Program.cs
+++ Cs11Dotnet7/Chapter04/WritingFunctions/Program.cs
@@ -1,3 +1,11 @@
 ﻿//TimesTable(2, size: 20);
 decimal taxToPay = CalculateTax(149, "FR");
 WriteLine("You must pay {0} in tax.", taxToPay);
+
+
+// converet numbers from cardinal to ordinal
+RunCardinaltoOrdinal();
+
+// factorial
+WriteLine("\n\n--- Factorial ---");
+RunFactorial();
