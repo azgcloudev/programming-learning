fca680c7ce56cd4955dab3caee19162ab36338f4
Aldair Zamora <aldair.zamora@outlook.com>
2023-10-23 11:04:11 -0600

    Review tracing and debug

---
+++ learning-cs/Cs11Dotnet7/Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
+
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+Console.WriteLine("Writing to: {0}", logPath);
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+// text writer is buffered, so this option calls
+// Flush() on all listeners after writing
+Trace.AutoFlush = true;
