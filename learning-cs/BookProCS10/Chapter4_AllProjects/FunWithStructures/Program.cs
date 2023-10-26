758d4cfd5aa2d65dd3cae3ca139619e33c24d957
Aldair Zamora <aldair.zamora@outlook.com>
2023-10-26 09:13:55 -0600

    Started to review structs

--- learning-cs/BookProCS10/Chapter4_AllProjects/FunWithStructures/Program.cs
+++ learning-cs/BookProCS10/Chapter4_AllProjects/FunWithStructures/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿Console.WriteLine("***** A first look at structures *****\n");
+
+// create an initial Point
+Point myPoint;
+myPoint.X = 349;
+myPoint.Y = 76;
+myPoint.Display();
+
+// adjust the X and Y values
+myPoint.Increment();
+myPoint.Display();
+Console.ReadLine();
+
+
+
+// =================================================
+
+// first struct
+struct Point
+{
+    // fields of the struct
+    public int X;
+    public int Y;
+
+    // Add 1 to the X Y position
+    public void Increment()
+    {
+        X++; Y++;
+    }
+
+    // substract 1 from X, Y position
+    public void Decrement()
+    {
+        X--; Y--;
+    }
+
+    // display the current position
+    public void Display()
+    {
+        Console.WriteLine("X = {0}, Y = {1}", X, Y);
+    }
+}
+
+
+// structures needs to use all variables in it, otherwise will get a compile error.
