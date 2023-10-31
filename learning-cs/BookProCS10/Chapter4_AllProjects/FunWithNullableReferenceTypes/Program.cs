8b4e5ac7d07c2af8409184b94e16c1e1687ebf11
Aldair Zamora <aldair.zamora@outlook.com>
2023-10-31 09:07:49 -0600

    Working with nullable

---
+++ learning-cs/BookProCS10/Chapter4_AllProjects/FunWithNullableReferenceTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FunWithNullableReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? nullableString = null;
+            TestClass? nullableClass = null;
+
+            TestClass myNonNullableClass = null;
+
+#nullable disable
+            TestClass anotherNullableClass = null;
+            TestClass? badDefinition = null;
+            string? anotherNullableString = null;
+#nullable restore
+            string? msg = null;
+            EnterLogData(msg);
+        }
+
+        static void EnterLogData(string message, string owner = "Programmer")
+        {
+            ArgumentNullException.ThrowIfNull(message);
+            Console.WriteLine("Error: {0}", message);
+            Console.WriteLine("Owner of Error: {0}", owner);
+        }
+    }
+
+    public class TestClass
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
--- learning-cs/BookProCS10/Chapter4_AllProjects/FunWithNullableValueTypes/Program.cs
+++ learning-cs/BookProCS10/Chapter4_AllProjects/FunWithNullableValueTypes/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FunWithNullableValueTypes
+﻿using System;
+
+namespace FunWithNullableValueTypes
 {
     class Program
     {
@@ -33,6 +35,30 @@
             {
                 Console.WriteLine("Value of 'b' is undefined.");
             }
+
+            //nullable coalescing operator
+            // assigns a value if the retrive value is null
+            DatabaseReader dr2 = new DatabaseReader();
+            int myData = dr2.GetIntFromDatabase() ?? 100;
+            Console.WriteLine("Value of myData: {0}.", myData);
+
+
+
+            // null coalescing assigment operator ??=
+            Console.WriteLine("\n\nNull coalescing assigment operator");
+            /**
+             * Assigns the right side value to the left side, only if the left side value is null.
+             */
+            int? nullInt = null;
+            nullInt ??= 12;
+            nullInt ??= 14;
+            Console.WriteLine(nullInt);
+
+            // null conditional
+            Console.WriteLine("\n\nnull conditional operator");
+            TesterMethod(null);
+
+            Console.ReadLine();
         }
 
         static void LocalNullableVariables()
@@ -46,6 +72,24 @@
 
             // nullable
             Nullable<int> age = null;
+        }
+
+        // ===== Null conditional operator =====
+        static void TesterMethod(string[] args)
+        {
+            // option 1, without null conditional operator
+            if (args != null)
+            {
+                Console.WriteLine("You sent me {0} arguments.", args.Length);
+            }
+            else
+            {
+                Console.WriteLine("You sent me {0} arguments.", 0);
+            }
+
+            // option 2, with conditional null operator 
+            Console.WriteLine("You sent me {0} arguments.", args?.Length ?? 0);
+
         }
     }
 
