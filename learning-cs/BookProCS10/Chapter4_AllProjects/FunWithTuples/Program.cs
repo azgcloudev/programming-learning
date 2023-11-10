39c9d4a06fa5a4934597c59b05e2e1d94136ccb8
Aldair Zamora <aldair.zamora@outlook.com>
2023-11-10 10:55:54 -0600

    working with tuples

--- learning-cs/BookProCS10/Chapter4_AllProjects/FunWithTuples/Program.cs
+++ learning-cs/BookProCS10/Chapter4_AllProjects/FunWithTuples/Program.cs
@@ -1,6 +1,8 @@
-﻿namespace FunWithTuples
+﻿using System.Collections.Generic;
+
+namespace FunWithTuples
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -44,8 +46,27 @@
             Console.WriteLine(samples.c);
 
             // discards
-            var (first,_,last) = SplitNames("Aldair Antonio Zamora");
+            var (first, _, last) = SplitNames("Aldair Antonio Zamora");
             Console.WriteLine($"{first}:{last}");
+
+
+            // tuples deconstruction
+            Console.WriteLine("\n\n-- tuples deconstruction --");
+            (int X, int Y) myTuple = (4, 5);
+            int x = 0;
+            int y = 0;
+            (x, y) = myTuple;
+            Console.WriteLine("X is: {0}", x);
+            Console.WriteLine("Y is: {0}", y);
+            (int x1, int y1) = myTuple;
+            Console.WriteLine($"x1 is: {x1}");
+            Console.WriteLine($"y1 is: {y1}");
+
+            // Deconstruct method
+            Console.WriteLine("\n\n-- Deconstruct method --");
+            Point myPoint = new Point(21, 95);
+            var customTypeTupleDecons = myPoint.Deconstruct();
+            Console.WriteLine(customTypeTupleDecons);
         }
 
         // static void FillTheseValues(out int a, out string b, out bool c)
@@ -64,6 +85,50 @@
             string[] words = fullName.Split(' ');
 
             return (words[0], words[1], words[2]);
+        }
+
+        // tuple pattern matching
+        static string RockPaperScissors(string first, string second)
+        {
+            return (first, second) switch
+            {
+                ("rock", "paper") => "Paper wins.",
+                ("rock", "scissors") => "Rock wins.",
+                ("paper", "rock") => "Paper wins.",
+                ("paper", "scissors") => "Scissors wins.",
+                ("scissors", "rock") => "Rock wins.",
+                ("scissors", "paper") => "Scissors wins.",
+                (_, _) => "Tie",
+            };
+        }
+
+        // tuple pattern matching passing tuple as parameter
+        static string RockPaperScissorsParams((string first, string second) value)
+        {
+            return value switch
+            {
+                ("rock", "paper") => "Paper wins.",
+                ("rock", "scissors") => "Rock wins.",
+                ("paper", "rock") => "Paper wins.",
+                ("paper", "scissors") => "Scissors wins.",
+                ("scissors", "rock") => "Rock wins.",
+                ("scissors", "paper") => "Scissors wins.",
+                (_, _) => "Tie",
+            };
+        }
+
+        struct Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int XPos, int YPos)
+            {
+                X = XPos;
+                Y = YPos;
+            }
+
+            public (int XPos, int YPos) Deconstruct() => (X, Y);
         }
     }
 }
