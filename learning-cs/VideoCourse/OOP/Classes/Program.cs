47a50190aeffef1f45d03f89ad08e47278c80342
Aldair Zamora <aldair.zamora@outlook.com>
2023-11-16 09:39:05 -0600

    Complete OOP section

--- learning-cs/VideoCourse/OOP/Classes/Program.cs
+++ learning-cs/VideoCourse/OOP/Classes/Program.cs
@@ -8,13 +8,27 @@
             // creating an object named audi, from the Car class
             Car audi = new Car("Audi", "350");
 
-            Car bmw = new Car("BMW", "450");
+            Car bmw = new Car("Bmw", "450");
 
             // using an object method
             audi.DrivingCar();
             bmw.DrivingCar();
             audi.Stop();
             bmw.Details();
+
+            Console.WriteLine(audi.GetHp());
+
+            // Accesing properties
+            Console.WriteLine("bmw name is: " + bmw.Name);
+            bmw.Name = "BMW";
+            Console.WriteLine("bmw name is: " + bmw.Name);
+
+            Console.WriteLine("Max speed: {0}.", bmw.MaxSpeed);
+            bmw.MaxSpeed = 100;
+            Console.WriteLine("Max speed: {0}.", bmw.MaxSpeed);
+
+
+
         }
     }
 }
