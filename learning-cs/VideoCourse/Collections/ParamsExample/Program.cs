a6b8127c8941db10d17730e511f7a48424a904e6
Aldair Zamora <aldair.zamora@outlook.com>
2023-11-29 10:56:49 -0600

    Working with params keyword

---
+++ learning-cs/VideoCourse/Collections/ParamsExample/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] test1 = ["hello", "world"];
+
+ParamsMethod(test1);
+
+// min number using params
+int[] numbers = {2345,1236,5467,2135,657,123};
+int minNumber = MinV2(numbers);
+Console.WriteLine(minNumber);
+
+Console.ReadKey();
+
+// ========  METHODS ==========
+static void ParamsMethod(params string[] words)
+{
+    for (int i = 0; i < words.Length; i++)
+    {
+        Console.Write("{0} ", words[i]);
+    }
+    Console.WriteLine();
+}
+
+static int MinV2(params int[] numbers)
+{
+    int min = int.MaxValue;
+
+    foreach (int num in numbers)
+    {
+        if (num < min)
+        {
+            min = num;
+        }
+    }
+
+    return min;
+}
