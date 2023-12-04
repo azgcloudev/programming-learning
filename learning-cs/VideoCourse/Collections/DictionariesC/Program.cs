1d282c76c5f753d713abcb5c849720b2f79ee279
Aldair Zamora <aldair.zamora@outlook.com>
2023-12-04 11:17:00 -0600

    Working with dictionaries

---
+++ learning-cs/VideoCourse/Collections/DictionariesC/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+/*
+ * Dictionaries are Generic collections and are also Key Value pairs
+ * Key values need to have a type
+ */
+
+// DECLARATION
+Dictionary<int, string> myDict = new Dictionary<int, string>
+{
+    {1, "one" },
+    {2, "two" },
+    {3, "three" }
+};
+
+// access an element
+Console.WriteLine(myDict[1]); // access by the key
+Console.WriteLine(myDict.ElementAt(2).Value); // access by index position
+
+// loop thru all elements
+foreach (KeyValuePair<int, string> item in myDict)
+{
+    Console.WriteLine($"Key: {item.Key} Value: {item.Value}");
+}
