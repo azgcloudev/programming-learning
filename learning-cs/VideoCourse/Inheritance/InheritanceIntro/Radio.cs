2c5663759f7c59ee6c512a9ef232c030f1a354b6
Aldair Zamora <aldair.zamora@outlook.com>
2023-12-16 11:29:50 -0600

    working with inheritance

---
+++ learning-cs/VideoCourse/Inheritance/InheritanceIntro/Radio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceIntro
+{
+    // CHILD CLASS FROM ELECTRCALDEVICE CLASS
+    internal class Radio : ElectricalDevice
+    {
+
+        public Radio(bool isOn, string brand) : base(isOn, brand) { }
+
+
+        public void ListenRadio()
+        {
+            if (IsOn == true)
+            {
+                Console.WriteLine("Listening to radio");
+            }
+        }
+    }
+
+}
---
+++ learning-cs/VideoCourse/Inheritance/InheritanceIntro/TV.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceIntro
+{
+    internal class TV : ElectricalDevice
+    {
+        public TV(bool isOn, string brand) : base(isOn, brand) { }
+
+        public void WatchTV()
+        {
+            if (IsOn == true)
+            {
+                Console.WriteLine("Watching TV");
+            }
+        }
+    }
+}
