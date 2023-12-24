3d268258b57c142253342097577af588d9eb9a42
Aldair Zamora <aldair.zamora@outlook.com>
2023-12-24 13:28:44 -0600

    Working with inheritance

--- learning-cs/VideoCourse/Inheritance/InheritanceIntro/Program.cs
+++ learning-cs/VideoCourse/Inheritance/InheritanceIntro/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using InheritanceIntro;
+
+Radio radio = new Radio(true, "panasonic");
+TV tv = new TV(false, "aoc");
+
+radio.ListenRadio();
+tv.WatchTV();
--- learning-cs/VideoCourse/Inheritance/InheritanceIntro/TV.cs
+++ learning-cs/VideoCourse/Inheritance/InheritanceIntro/TV.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InheritanceIntro
+﻿namespace InheritanceIntro
 {
     internal class TV : ElectricalDevice
     {
@@ -14,7 +8,7 @@ namespace InheritanceIntro
         {
             if (IsOn == true)
             {
-                Console.WriteLine("Watching TV");
+                Console.WriteLine("Watching TV"); 
             }
         }
     }
