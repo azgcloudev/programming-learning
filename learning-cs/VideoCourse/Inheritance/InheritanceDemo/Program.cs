9a3abc532cf63aa25768e1e47760c9033b44fe7d
Aldair Zamora <aldair.zamora@outlook.com>
2024-01-10 10:42:40 -0600

    Working in inheritance challange

--- learning-cs/VideoCourse/Inheritance/InheritanceDemo/Program.cs
+++ learning-cs/VideoCourse/Inheritance/InheritanceDemo/Program.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-using InheritanceDemo;
+﻿using InheritanceDemo;
 
 Post post1 = new Post("Thanks for the birthday wishes.", "Aldair Zamora", true);
 ImagePost imagePost1 = new ImagePost("Checkout my new shoes", "Aldair Zamora", true, "https://myimage.local");
 
 Console.WriteLine(post1.ToString());
 Console.WriteLine(imagePost1.ToString());
+
+VideoPost videoPost1 = new VideoPost("Paris trip", "Aldair", true, "https://myvideo.com", 20);
+Console.WriteLine(videoPost1.ToString());
 
 Console.ReadLine(); 
---
+++ learning-cs/VideoCourse/Inheritance/InheritanceDemo/VideoPost.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    /*
+     create a VideoPost with properties VideoURL, Length
+
+     create constructors
+     adjust the ToString() method accordingly
+     create a instance of VideoPost class
+
+     MORE ADVANCE STEPS:
+      use Timer an Callback method here
+      create fields as  quirered
+      add members method "Play" which shuld write the current duration of the video
+      and "Stop" which should stop the "timer" and write "stoped at {0}s" onto the console.
+      Play the video after creating the instance and pause it, when the user presses any key
+     */
+
+    class VideoPost : Post
+    {
+
+        public string? VideoURL { get; set; }
+        public int Length { get; set; }
+
+        // constructor
+        public VideoPost() { }
+        public VideoPost(string title, string sendByUsername, bool isPublic, string videoURL, int length)
+        {
+            // properties from Post class
+            ID = GetNextID();
+            Title = title;
+            SendByUsername = sendByUsername;
+            IsPublic = isPublic;
+
+            VideoURL = videoURL;
+            Length = length;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - by {3}", ID, Title, VideoURL, SendByUsername);
+        }
+    }
+}
