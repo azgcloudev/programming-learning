6751dd1b1bb7578e469a9d593c528471aa078f63
Aldair Zamora <aldair.zamora@outlook.com>
2024-01-19 10:58:00 -0600

    first view of interfaces

---
+++ learning-cs/VideoCourse/Interfaces/InterfacesDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(11);
+
+            Console.WriteLine(t1.Equals(t2));
+        }
+    }
+}
---
+++ learning-cs/VideoCourse/Interfaces/InterfacesDemo/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    class Ticket : IEquatable<Ticket>
+    {
+        public int DurationInHours { get; set; }
+
+        public Ticket(int durationInHours)
+        {
+            DurationInHours = durationInHours;
+        }
+
+        // interface method
+        public bool Equals(Ticket other)
+        {
+            return DurationInHours == other.DurationInHours;
+        }
+    }
+}
