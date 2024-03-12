f19f20ac3701f1cfbfb1889b62720478baac7398
Aldair Zamora <aldairzamora@microsoft.com>
2024-03-11 20:18:13 -0600

    update project

--- learning-go/Course/helloworld/main.go
+++
@@ -1,8 +0,0 @@
-// package is a collection of source files
-package main // only package main will create an executable
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hi there!")
-}
--- learning-go/VideoCourse/application_structure/go.mod
+++
@@ -1,3 +0,0 @@
-module application_structure
-
-go 1.21.0
