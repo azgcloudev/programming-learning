f5e1bae9e6657567486e63014252b26698c59ee4
Aldair Zamora <aldair.zamora@outlook.com>
2024-03-13 12:18:21 -0600

    study

---
+++ learning-cs/VideoCourse/Section10Assigment2/Section10Assigment2/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] lines = File.ReadAllLines(@"D:\Programming\Learning\programming-learning\learning-cs\VideoCourse\Section10Assigment2\input.txt");
+List<string> splittedLines = new List<string> { };
+List<string> splittedText = new List<string> { };
+
+foreach (string line in lines)
+{
+    if (line.Contains("split"))
+    {
+        splittedLines.Add(line);
+    }
+}
+
+foreach(string text in splittedLines)
+{
+    Console.WriteLine(text);
+}
