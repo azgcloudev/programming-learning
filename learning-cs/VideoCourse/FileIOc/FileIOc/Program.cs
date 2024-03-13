e10a01e08a7c4179088c7489cf5315958796d1ee
Aldair Zamora <aldair.zamora@outlook.com>
2024-03-13 08:28:53 -0600

    writing on files

--- learning-cs/VideoCourse/FileIOc/FileIOc/Program.cs
+++ learning-cs/VideoCourse/FileIOc/FileIOc/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            string path = "D:\\Documents\\Programming\\Learning\\programming-learning\\learning-cs\\VideoCourse\\FileIOc\\FileIOc\\";
+
             // all text
             string allText = System.IO.File.ReadAllText(@"D:\Documents\Programming\Learning\programming-learning\learning-cs\VideoCourse\FileIOc\FileIOc\textFile.txt");
 
@@ -14,11 +16,44 @@
             string[] lines = System.IO.File.ReadAllLines(@"D:\Documents\Programming\Learning\programming-learning\learning-cs\VideoCourse\FileIOc\FileIOc\textFile.txt");
 
             Console.WriteLine("Single lines:");
-            foreach(string line in lines)
+            foreach (string line in lines)
             {
                 Console.WriteLine("\t{0}", line);
             }
 
+            // Write in a file
+            string[] linesToWrite = { "first line", "second line", "third line." };
+            File.WriteAllLines(@"D:\Documents\Programming\Learning\programming-learning\learning-cs\VideoCourse\FileIOc\FileIOc\textFile2.txt", linesToWrite);
+
+            // practice
+            int high_score = int.MinValue;
+
+            int[] scores = { 123, 432, 63 };
+
+            foreach (int score in scores)
+            {
+                if (score > high_score)
+                {
+                    high_score = score;
+                }
+            }
+
+            // write the score in the file
+            File.WriteAllText(@"D:\Documents\Programming\Learning\programming-learning\learning-cs\VideoCourse\FileIOc\FileIOc\highScore.txt", high_score.ToString());
+
+
+            string[] lines3 = { "First 250", "Second 100", "Third 240", "Second 367" };
+            // wirte using streamwriter
+            using (StreamWriter file = new StreamWriter(@"D:\Documents\Programming\Learning\programming-learning\learning-cs\VideoCourse\FileIOc\FileIOc\myText2.txt"))
+            {
+                foreach (string line in lines3)
+                {
+                    if (line.Contains("Second"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
             Console.ReadKey();
         }
     }
