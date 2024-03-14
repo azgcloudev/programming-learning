fb40ad62a617b922da27782d63900a0a1adf4d81
Aldair Zamora <aldair.zamora@outlook.com>
2024-03-14 10:10:07 -0600

    review of enums

---
+++ learning-cs/VideoCourse/AdvanceTopics/EnumsC/EnumsC/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EnumsC
+{
+    enum Months { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+    // by default start with 0
+    // we can set the int value of the first element
+    // any other element can be change its number and the next ones will start from that last value
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ENUMS
+            Months June = Months.Jun;
+            Console.WriteLine($"My month of birth us {June}, month number {(int)June}.");
+        }
+    }
+}
