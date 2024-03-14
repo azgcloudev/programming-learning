9a64adfb6a5eb4c87d037474f5ca2b1cea651fdf
Aldair Zamora <aldair.zamora@outlook.com>
2024-03-14 09:50:46 -0600

    review of structs

---
+++ learning-cs/VideoCourse/AdvanceTopics/StructsC/StructsC/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StructsC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // STRUCTS
+            Game game1;
+            game1.Name = "Test";
+            game1.Rating = 8.2;
+            // game1.Display(); // cannot be used since not all the properties have a value yet
+            game1.Force = 79;
+            game1.Display();
+        }
+
+        private struct Game
+        {
+            public string Name;
+            public int Force;
+            public double Rating;
+
+            public void Display()
+            {
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Force: {Force}");
+                Console.WriteLine($"Rating: {Rating}");
+            }
+        }
+    }
+}
