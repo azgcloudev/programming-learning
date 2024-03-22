413a47fd94b4ed817bf60f0823b2de21a58dee92
Aldair Zamora <aldair.zamora@outlook.com>
2024-03-22 08:41:08 -0600

    study regex

--- learning-cs/VideoCourse/AdvanceTopics/RegexT/RegexT/Program.cs
+++ learning-cs/VideoCourse/AdvanceTopics/RegexT/RegexT/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+// match german phone numbers
+string phoneNumberRegexPattern = @"(\+49\d{3}|0049\d{3}|0\d{3})/\d{8}"; // ^(\+49\d{3}|0049\d{3}|0\d{3})/\d{8}
+
+Regex regex = new Regex(phoneNumberRegexPattern);
+
+string text = "Hello my number is 0049165/12312347";
+
+// **** mr and ms match ****
+// (mr|ms)\.?\s[A-Z]*
+
+MatchCollection matches = regex.Matches(text);
+
+Console.WriteLine(matches.Count);
+Console.WriteLine($"Phone number: {matches.ElementAt(0).Value}");
