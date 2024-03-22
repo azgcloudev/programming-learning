7429bae2404368139c768ce3855dc4be004408b7
Aldair Zamora <aldair.zamora@outlook.com>
2024-03-22 11:00:06 -0600

    working with DateTime

---
+++ learning-cs/VideoCourse/AdvanceTopics/DateTimeT/DateTimeT/Program.cs
@@ -0,0 +1,17 @@
+﻿DateTime dateTime = new DateTime(2002, 3, 4);
+
+Console.WriteLine(dateTime);
+
+//current day of today
+Console.WriteLine("Today is: {0}", DateTime.Today);
+Console.WriteLine(DateTime.Now); //current time in machine
+
+// following day
+DateTime tomorrow = GetTomorrow();
+Console.WriteLine("Tomorrow is {0}.", tomorrow);
+
+
+static DateTime GetTomorrow()
+{
+    return DateTime.Today.AddDays(1);
+}
