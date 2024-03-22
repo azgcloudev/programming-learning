82e37ba97429a4d7b8c7716dc6cff194abfc4ed3
Aldair Zamora <aldair.zamora@outlook.com>
2024-03-21 22:27:44 -0600

    review dictionaries

--- learning-py/helloWorld/dictionaries.py
+++ learning-py/helloWorld/dictionaries.py
@@ -1,2 +1,24 @@
 # DICTIONARY
-# they are changable, unordered and uses key value pairs
+# they are changeable, unordered and uses key value pairs
+# dicts are fast because they use hashing
+
+capitals = {
+    "Costa Rica": "San Jose",
+    "Norway": "Oslo",
+    "Spain": "Madrid"
+}
+
+print(capitals["Norway"])
+print(capitals.get("Spain"))
+print(capitals.keys())
+print(capitals.values())
+print(capitals.items())
+
+capitals.update({"Germany": "Berlin"})
+
+print()
+for key, value in capitals.items():
+    print(key + " " + value)
+
+capitals.pop("Spain")
+capitals.clear()
