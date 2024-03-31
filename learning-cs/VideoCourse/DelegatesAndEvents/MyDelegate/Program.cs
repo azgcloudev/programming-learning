105e78c3567343c52914ddd7ca93b1e96c1d9af9
Aldair Zamora <aldair.zamora@outlook.com>
2024-03-30 23:04:50 -0600

    delegates creation

---
+++ learning-cs/VideoCourse/DelegatesAndEvents/MyDelegate/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace MyDelegate
+{
+    internal class Program
+    {
+        // Defining a delegate type called FilterDelegate that takes a person object
+        // and returns a bool
+        public delegate bool FilterDelegate(Person p);
+
+        static void Main(string[] args)
+        {
+            // create 4 person objects
+            Person p1 = new Person() { Name = "Aiden", Age = 41 };
+            Person p2 = new Person() { Name = "Sif", Age = 69 };
+            Person p3 = new Person() { Name = "Walter", Age = 12 };
+            Person p4 = new Person() { Name = "Anatoli", Age = 25 };
+
+            // add the objects to a list called people
+            List<Person> people = new List<Person>() { p1, p2, p3, p4 };
+
+            DisplayPeople("Kids", people, IsMinor);
+            DisplayPeople("Adults", people, IsAdult);
+            DisplayPeople("Seniors", people, IsSenior);
+        }
+
+        public static void DisplayPeople(string title, List<Person> list, FilterDelegate filter)
+        {
+            Console.WriteLine($"{title}:");
+
+            foreach (Person p in list)
+            {
+                if (filter(p))
+                {
+                    Console.WriteLine($"{p.Name}, {p.Age} years old");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        // ================ FILTERS =======================
+        static bool IsMinor(Person p)
+        {
+            return p.Age < 18;
+        }
+
+        static bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+
+        static bool IsSenior(Person p)
+        {
+            return p.Age >= 65;
+        }
+    }
+}
