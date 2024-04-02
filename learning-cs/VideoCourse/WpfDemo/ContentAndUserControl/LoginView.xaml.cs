6c017ffd4c26cf24fab2925f1f1e01543e21109e
Aldair Zamora <aldair.zamora@outlook.com>
2024-04-01 23:16:44 -0600

    working with user and content control

---
+++ learning-cs/VideoCourse/WpfDemo/ContentAndUserControl/LoginView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContentAndUserControl
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        // Login button
+        private void ButtonLogin_Clicked(object sender, RoutedEventArgs e)
+        {
+            // Call the window object to changes its content
+            Window window = Window.GetWindow(this);
+            window.Content = new InvoiceView();
+        }
+    }
+}
