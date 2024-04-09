087b3d37b74ac83c3e5eb97b7fbf0453114c1390
Aldair Zamora <aldair.zamora@outlook.com>
2024-04-08 23:20:47 -0600

    remove c

---
+++ learning-c/learning-c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  learning-c
+//
+//  Created by Aldair Zamora on 6/4/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
