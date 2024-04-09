17393ea26cdeb1cf295b84b978179795dcaefdca
Aldair Zamora <aldair.zamora@outlook.com>
2024-04-09 10:18:05 -0600

    developing currency converter

--- learning-cs/VideoCourse/CurrencyConverterDesktop/CurrencyConverterDesktop/MainWindow.xaml.cs
+++ learning-cs/VideoCourse/CurrencyConverterDesktop/CurrencyConverterDesktop/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,61 @@ namespace CurrencyConverterDesktop
         public MainWindow()
         {
             InitializeComponent();
+            BindCurrency();
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            // store the converted value of the currency
+            double convertedValue;
+
+            // check if the amount box is null or blank
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtCurrency.Focus();
+                return;
+            }
+            // check if From currency box is default text or not selected
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            lblCurrency.Content = "";
+        }
+
+        private void NumberValidationTextBox(object sender, RoutedEventArgs e) { }
+
+        private void BindCurrency()
+        {
+            DataTable dtCurrency = new DataTable();
+
+            // create columns
+            dtCurrency.Columns.Add("Text");
+            dtCurrency.Columns.Add("Value");
+
+            // add rows to the table
+            dtCurrency.Rows.Add("---SELECT---", 0);
+            dtCurrency.Rows.Add("INT", 1);
+            dtCurrency.Rows.Add("USD", 75);
+            dtCurrency.Rows.Add("EUR", 85);
+            dtCurrency.Rows.Add("SAR", 20);
+            dtCurrency.Rows.Add("POUND", 5);
+            dtCurrency.Rows.Add("DEM", 43);
+
+            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbFromCurrency.DisplayMemberPath = "Text";
+            cmbFromCurrency.SelectedValuePath = "Value";
+            cmbFromCurrency.SelectedIndex = 0;
+
+            cmbToCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
         }
     }
 }
