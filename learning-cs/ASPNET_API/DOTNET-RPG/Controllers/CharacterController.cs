1ffb7afbc2585ac3e3ccf8f2b8a9669ebb313232
Aldair Zamora <aldair.zamora@outlook.com>
2024-04-10 00:28:57 -0600

    initial review of asp.net web api controller

---
+++ learning-cs/ASPNET_API/DOTNET-RPG/Controllers/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using DOTNET_RPG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOTNET_RPG.Controllers
+{
+
+    [ApiController] //api controller attribute
+    [Route("api/[controller]")] // route attribute
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character {Name = "Sam", Id = 1}
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        // function that gets the first item
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
