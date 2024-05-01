ba6d49ad419715a98712b58c3eff7e65584aa8b7
Aldair Zamora <aldair.zamora@outlook.com>
2024-04-30 23:15:21 -0600

    first linq example

---
+++ learning-cs/VideoCourse/Linq/Linq1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+OddNumbers(numbers);
+
+Console.ReadKey();
+// end of program
+
+static void OddNumbers(int[] numbers)
+{
+    Console.WriteLine("Odd numbers:");
+
+    IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number; // linq statement
+
+    foreach(int num in oddNumbers)
+    {
+        Console.WriteLine(num);
+    }
+}
