d970e5810779c7ddac9f711b21b779cf4f6b0f26
Aldair Zamora <aldair.zamora@outlook.com>
2024-05-03 09:56:06 -0600

    linq with sql

---
+++ learning-cs/VideoCourse/Linq/LinqToSQL/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace LinqToSQL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        LinqToSQLDataClassesDataContext dataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.PanjutorialsDBConnectionString"].ConnectionString;
+            dataContext = new LinqToSQLDataClassesDataContext(connectionString);
+
+            InsertUniversity();
+        }
+
+        // insert a university into a table
+        public void InsertUniversity()
+        {
+            University yale = new University();
+            University beijinTech = new University() { Name = "Beijin Tech" };
+
+            yale.Name = "Yale";
+
+            dataContext.Universities.InsertOnSubmit(yale);
+            dataContext.Universities.InsertOnSubmit(beijinTech);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+    }
+}
