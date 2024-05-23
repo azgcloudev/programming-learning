b3353349c6605e7043ebebf8b1d9eee077615aec
Aldair Zamora <aldair.zamora@outlook.com>
2024-05-23 09:12:30 -0600

    SOLID review of liskov principal

---
+++ learning-cs/VideoCourse/CleanCode/L_SOLID/Penguin.cs
@@ -0,0 +1,6 @@
+namespace L_SOLID;
+
+public class Penguin : Bird
+{
+    
+}
---
+++ learning-cs/VideoCourse/CleanCode/L_SOLID/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace L_SOLID;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       /*
+        * LISKOV SUSTITUTION PRINCIPLE
+        * says that we should be able to replace our derive classes without the base class
+        */
+       Sparrow sparrow1 = new Sparrow();
+       sparrow1.Fly();
+       
+       Bird sparrow = new Sparrow();
+       ((IFlyable)sparrow).Fly(); // as it is created with Bird class, needs to cast the object with the Interface that inplements the method
+
+       Bird penguin = new Penguin();
+       penguin.MakeSound();
+
+    }
+}
