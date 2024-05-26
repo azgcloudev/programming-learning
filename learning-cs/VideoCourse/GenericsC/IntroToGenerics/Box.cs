7882d215492e71b2fed226a4be7570413d452e55
Aldair Zamora <aldair.zamora@outlook.com>
2024-05-26 00:09:00 -0600

    intro to generics

---
+++ learning-cs/VideoCourse/GenericsC/IntroToGenerics/Box.cs
@@ -0,0 +1,13 @@
+namespace IntroToGenerics;
+
+// creating a Generic class
+public class Box<T>
+{
+    // Content will get the type 
+    public T? Content { get; set; }
+
+    public string Log()
+    {
+        return $"Box contains {Content}";
+    }
+}
---
+++ learning-cs/VideoCourse/GenericsC/IntroToGenerics/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IntroToGenerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> books = new List<string>();
+        
+        Box<int> boxInt = new Box<int>();
+        boxInt.Content = 2;
+
+        Box<string> boxStr = new Box<string>();
+        boxStr.Content = "Regalos";
+
+        Console.WriteLine(boxInt.Log());
+        Console.WriteLine(boxStr.Log());
+
+        Bag<string> bag = new Bag<string>("Pali");
+        bag.UpdateContent("Walmart");
+        Console.WriteLine(bag.GetContent());
+    }
+}
