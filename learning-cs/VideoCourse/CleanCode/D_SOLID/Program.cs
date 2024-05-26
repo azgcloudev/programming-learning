f95a32312a3d1fa530f5bec6b8ccf748ed582d68
Aldair Zamora <aldair.zamora@outlook.com>
2024-05-25 20:05:07 -0600

    DIP = dependency inversion principle

---
+++ learning-cs/VideoCourse/CleanCode/D_SOLID/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace D_SOLID;
+
+// DEPENDENCY INVERSION PRINCIPLE = DIP
+/*
+ * It says that higher lever classes should not depend on lower level classes.
+ * Meaning that in a class you should not create an object of the lower class.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        Notification notification = new Notification(new EmailService());
+        notification.Send("Test");
+    }
+}
