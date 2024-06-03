fcb39ecacffb5cb11e658385b5694469513c45c2
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-03 10:49:39 -0600

    generic interfaces

---
+++ learning-cs/VideoCourse/GenericsC/GenericInterfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericInterfaces
+{
+    internal interface IRepository<T>
+    {
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
