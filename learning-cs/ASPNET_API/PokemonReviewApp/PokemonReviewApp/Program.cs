16b807b5e557f45fede05a655e45d582e5aaf383
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-03 08:37:23 -0600

    working with aspnet

--- learning-cs/ASPNET_API/PokemonReviewApp/PokemonReviewApp/Program.cs
+++ learning-cs/ASPNET_API/PokemonReviewApp/PokemonReviewApp/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using PokemonReviewApp.Data;
 
 namespace PokemonReviewApp;
 
@@ -13,11 +17,34 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
+        builder.Services.AddTransient<Seed>();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        // dbcontext
+        builder.Services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
         var app = builder.Build();
+
+        if (args.Length == 1 && args[0].ToLower() == "seeddata")
+        {
+            SeedData(app);
+        }
+
+        void SeedData(IHost app)
+        {
+            var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+            using (var scope = scopedFactory.CreateScope())
+            {
+                var service = scope.ServiceProvider.GetService<Seed>();
+                service.SeedDataContext();
+            }
+        }
 
         // Configure the HTTP request pipeline. 
         if (app.Environment.IsDevelopment())
