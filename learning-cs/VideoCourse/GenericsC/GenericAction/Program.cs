da5b6c6bdee33031f8c0833e937197b4c7e578dd
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-10 09:44:12 -0600

    generic action delegate

---
+++ learning-cs/VideoCourse/GenericsC/GenericAction/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericAction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Action do not return anything
+            // but it takes parameters
+            Action action = () =>
+            {
+                Console.WriteLine("This print comes from the a Generic action<>");
+            };
+
+            Action<string> displayName = name =>
+            {
+                Console.WriteLine($"Hello {name} welcome to Generics");
+            };
+
+            action();
+            displayName("Aldair");
+        }
+    }
+}
