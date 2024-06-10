e70b41abf61a7b8d283a559512933a80581bd6af
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-09 18:09:48 -0600

    studying

--- learning-cs/dotnet-maui-workshop-main/Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ learning-cs/dotnet-maui-workshop-main/Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,54 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel()
+    {
+    }
+
+    [ObservableProperty] [AlsoNotifyChangeFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty] string title;
+
+    // manual version to implement INotifyPropertyChanged
+    /*
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+
+            isBusy = value;
+            OnPropertyChanged(nameof(IsBusy));
+            OnPropertyChanged(nameof(IsNotBusy));
+        }
+    }
+
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value)
+                return;
+
+            title = value;
+            OnPropertyChanged(nameof(Title));
+        }
+    }
+
+    public bool IsNotBusy => IsBusy;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+    */
+
+    public bool IsNotBusy => IsBusy;
 }
