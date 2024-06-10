9a94c909b5af542fd1eb931c7daed905058c3a86
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-10 10:19:59 -0600

    generic predicate and function delegates

---
+++ learning-cs/VideoCourse/GenericsC/GenericFunctionDelegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace GenericFunctionDelegate
+{
+    internal class Program
+    {
+        private const int CurrentYear = 2024;
+
+        static void Main(string[] args)
+        {
+            // function<> does provide a return type
+
+            Func<int, int> getAgeByYear = (birthYear) => CurrentYear - birthYear;
+
+            Console.WriteLine("What is your birth year? ");
+            int birthYear;
+            int age = -1;
+
+            try
+            {
+                birthYear = int.Parse(Console.ReadLine());
+                age = getAgeByYear(birthYear);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Unable to parse input");
+            }
+
+            if (age != -1)
+            {
+                Console.WriteLine("Current age is {0}", age);
+            }
+            else
+            {
+                Console.WriteLine("Not a valid number input, unable to calculate age.");
+            }
+        }
+    }
+}
---
+++ learning-cs/VideoCourse/GenericsC/GenericPredicateDelegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericPredicateDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = number => number % 2 == 0;
+
+            // use the predicate when a function allows it
+            // like the following
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var evenNumbers = numbers.FindAll(isEven);
+
+            Console.Write("[ ");
+            foreach (var number in evenNumbers)
+            {
+                Console.Write("{0} ", number);
+            }
+            Console.Write("]");
+        }
+    }
+}
