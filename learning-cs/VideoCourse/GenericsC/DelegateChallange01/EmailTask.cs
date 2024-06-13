f7ef76266d6e9d1ee6655f86f6395ae3dae7222e
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-13 13:29:37 -0600

    complete challange

---
+++ learning-cs/VideoCourse/GenericsC/DelegateChallange01/EmailTask.cs
@@ -0,0 +1,13 @@
+﻿namespace DelegateChallange01
+{
+    internal class EmailTask : ITask<string>
+    {
+        private string Recipient { get; set; } = "aldair@azgcloudev.xyz";
+        private string Message { get; set; } = "Test message";
+
+        public string Perform()
+        {
+            return $"Email sent to {Recipient}, with message {Message}";
+        }
+    }
+}
---
+++ learning-cs/VideoCourse/GenericsC/DelegateChallange01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DelegateChallange01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailTask emailTask = new EmailTask();
+            ReportTask reportTask = new ReportTask();
+
+            TaskProcessor<EmailTask, string> taskProcessorEmail = new TaskProcessor<EmailTask, string>(emailTask);
+            TaskProcessor<ReportTask, string> taskProcessorReportTask =
+                new TaskProcessor<ReportTask, string>(reportTask);
+
+            Console.WriteLine(taskProcessorEmail.Execute());
+            Console.WriteLine(taskProcessorReportTask.Execute());
+        }
+    }
+}
