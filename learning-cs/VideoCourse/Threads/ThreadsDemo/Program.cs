d5dd15ef727a4ee8f6e54bb9e86d55d5dc139a55
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-13 14:40:50 -0600

    threads

---
+++ learning-cs/VideoCourse/Threads/ThreadsDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ThreadsDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // the following runs in the single MAIN thread
+        /*
+        Console.WriteLine("Hello 1");
+        Thread.Sleep(2000);
+        Console.WriteLine("Hello 2");
+        Thread.Sleep(2000);
+        Console.WriteLine("Hello 3");
+        Thread.Sleep(2000);
+        Console.WriteLine("Hello 4");
+        */
+
+        // running multiple threads
+        // they will run at the same time, not necessary in the same order
+        new Thread(() =>
+        {
+            Console.WriteLine("Thread 1");
+            Thread.Sleep(1000);
+            Console.WriteLine("Thread 1 ended");
+        }).Start();
+        new Thread(() =>
+        {
+            Console.WriteLine("Thread 2");
+            Thread.Sleep(1000);
+            Console.WriteLine("Thread 2 ended");
+        }).Start();
+        new Thread(() =>
+        {
+            Console.WriteLine("Thread 3");
+            Thread.Sleep(1000);
+            Console.WriteLine("Thread 3 ended");
+        }).Start();
+        new Thread(() =>
+        {
+            Console.WriteLine("Thread 4");
+            Thread.Sleep(1000);
+            Console.WriteLine("Thread 4 ended");
+        }).Start();
+
+        Console.ReadKey();
+    }
+}
