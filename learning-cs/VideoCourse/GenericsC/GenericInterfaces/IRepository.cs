2519ef44c556f28681ccc97e2be7668639035978
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-13 13:29:44 -0600

    generics

--- learning-cs/VideoCourse/GenericsC/GenericInterfaces/IRepository.cs
+++ learning-cs/VideoCourse/GenericsC/GenericInterfaces/IRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenericInterfaces
+﻿namespace GenericInterfaces
 {
     internal interface IRepository<T> where T : IEntity // implments that and Id propertity must exists
     {
