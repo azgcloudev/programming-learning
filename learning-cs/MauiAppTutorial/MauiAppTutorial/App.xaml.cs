e92a0b47c4eb1a47d6522da939746b5691a5cae2
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-26 08:17:21 -0600

    add maui tutorial project

---
+++ learning-cs/MauiAppTutorial/MauiAppTutorial/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppTutorial
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ learning-cs/MauiAppTutorial/MauiAppTutorial/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppTutorial
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
