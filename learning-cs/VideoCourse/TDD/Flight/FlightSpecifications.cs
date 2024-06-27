71d3fb250b539c19a371d150695fbb4c5ac0b267
Aldair Zamora <aldair.zamora@outlook.com>
2024-06-27 10:56:05 -0600

    tdd

---
+++ learning-cs/VideoCourse/TDD/Flight/FlightSpecifications.cs
@@ -0,0 +1,31 @@
+using Domain;
+using FluentAssertions;
+
+namespace FlightTest
+{
+    public class FlightSpecifications
+    {
+        [Fact]
+        public void Booking_reduces_the_number_of_seats()
+        {
+            var flight = new Flight(seatCapacity: 3);
+
+            flight.Book("aldair@azgcloudev.xyz", 1);
+            flight.RemainingNumberOfSeats.Should().Be(2);
+
+        }
+
+        [Fact]
+        public void Avoids_overbooking()
+        {
+            // GIVEN
+            Flight flight = new Flight(3);
+
+            // WHEN
+            var error = flight.Book("aldair@azgcloudev.xyz", 4);
+
+            // THEN
+            error.Should().BeOfType<OverBookingError>();
+        }
+    }
+}
