a88a452adf562a0318f507017e1ef80ae95717c1
Aldair Zamora <aldair.zamora@outlook.com>
2024-07-29 11:01:05 -0600

    delegates multicasting

---
+++ learning-cs/Book/Chapter12/Delegates/CarDelegate/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace CarDelegate
+{
+    internal class Car
+    {
+        // internal state data
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        
+        // is the car alive or dead
+        private bool _carIsDead;
+
+
+        // Class instructors
+        public Car()
+        {
+        }
+
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        // 1) define the delegate type
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        // 2) Define a member value for the delegate
+        private CarEngineHandler _listOfHandlers;
+
+        // 3) Add registration function for the caller
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            //_listOfHandlers = methodToCall; // add a single method
+
+            // multicasting to append multiple methods
+            _listOfHandlers += methodToCall;
+        }
+
+        // 4) Implement the Accelerate() method to invoke the delegate's invocation list
+        // under the correct circumstances
+        public void Accelerate(int delta)
+        {
+            // if car is dead, send dead message
+            if (_carIsDead)
+            {
+                _listOfHandlers?.Invoke("Sorry, this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                // is the car almost dead?
+                if (10 == (MaxSpeed - CurrentSpeed))
+                {
+                    _listOfHandlers?.Invoke("Careful buddy! Gonna blow!");
+                }
+
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    _carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ learning-cs/Book/Chapter12/Delegates/CarDelegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CarDelegate;
+
+Console.WriteLine("** Delegates as event enablers **\n");
+
+// make the car object
+var c1 = new Car("Slugbug", 100, 10);
+
+// REGISTER MULTIPLE TARGET NOTIFICATIONS (MULTICASTING delegates)
+// now, tell the car which method to call when it wants to send us a message
+c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent2));
+
+// speed up (this will trigger events)
+Console.WriteLine("***** Speeding up *****");
+for (int i = 0; i < 9; i++)
+{
+    c1.Accelerate(20);
+}
+Console.ReadLine();
+
+// this is the target for incoming events
+static void OnCarEngineEvent(string msg)
+{
+    Console.WriteLine("\n*** Message from Car object ***");
+    Console.WriteLine("=> {0}", msg);
+    Console.WriteLine("********************\n");
+}
+
+// second event
+static void OnCarEngineEvent2(string msg)
+{
+    Console.WriteLine("--> {0}", msg.ToUpper());
+}
