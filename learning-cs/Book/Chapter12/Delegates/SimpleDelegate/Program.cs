be2c129105028f530853d26eb2a12c46ea54e057
Aldair Zamora <aldair.zamora@outlook.com>
2024-07-29 10:00:09 -0600

    complete SimpleDelegate

--- learning-cs/Book/Chapter12/Delegates/SimpleDelegate/Program.cs
+++ learning-cs/Book/Chapter12/Delegates/SimpleDelegate/Program.cs
@@ -10,19 +10,35 @@ namespace SimpleDelegate
         {
             Console.WriteLine("***** Simple Delegate Example *****\n");
 
+            SimpleMath m = new SimpleMath();
+
             // Create a BinaryOp delegate object that "points to" Simplemath.Add();
-            BinaryOp b = new BinaryOp(SimpleMath.Add);
+            BinaryOp b = new BinaryOp(m.Add);
 
             // the following makes a compile error, because the method doesn't follow
             // the delegate parameters:
             /*
              * error CS0123: No overload for 'SquareNumber' matches delegate 'Program.BinaryOp'
-                BinaryOp s = SimpleMath.SquareNumber;
+                BinaryOp s = m.SquareNumber;
              */
 
             // invoke the Add() method indirectly using the delegate object
             Console.WriteLine("10 + 10 is {0}", b(10,10));
             Console.ReadLine();
+
+
+            // display delagte info
+            DisplayDelegateInfo(b);
+        }
+
+        static void DisplayDelegateInfo(Delegate delObj)
+        {
+            // print the name of each member in the delegate's invocation list
+            foreach (Delegate d in delObj.GetInvocationList())
+            {
+                Console.WriteLine("Method name: {0}", d.Method);
+                Console.WriteLine("Type Name: {0}", d.Target);
+            }
         }
     }
 }
--- learning-cs/Book/Chapter12/Delegates/SimpleDelegate/SimpleMath.cs
+++ learning-cs/Book/Chapter12/Delegates/SimpleDelegate/SimpleMath.cs
@@ -8,14 +8,14 @@ namespace SimpleDelegate
 {
     internal class SimpleMath
     {
-        public static int Add(int x, int y) => x + y;
-        public static int Subtract(int x, int y) => x - y;
+        public int Add(int x, int y) => x + y;
+        public int Subtract(int x, int y) => x - y;
 
         /// <summary>
         /// Return the square of a given number.
         /// </summary>
         /// <param name="a">Integer to get the square.</param>
         /// <returns>Returns an integer</returns>
-        public static int SquareNumber(int a) => a * a;
+        public int SquareNumber(int a) => a * a;
     }
 }
