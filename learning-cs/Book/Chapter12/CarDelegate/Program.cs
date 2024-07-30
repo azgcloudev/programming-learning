84c9c04608844f6ea676eb61aefb24115fca1a75
Aldair Zamora <aldair.zamora@outlook.com>
2024-07-29 23:04:49 -0600

    method group conversion syntax

--- learning-cs/Book/Chapter12/CarDelegate/Program.cs
+++ learning-cs/Book/Chapter12/CarDelegate/Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("** Delegates as event enablers **\n");
 
 // make the car object
 var c1 = new Car("Slugbug", 100, 10);
+var c2 = new Car();
 
 // REGISTER MULTIPLE TARGET NOTIFICATIONS (MULTICASTING delegates)
 // now, tell the car which method to call when it wants to send us a message
@@ -14,12 +15,20 @@ Car.CarEngineHandler handler2 = new Car.CarEngineHandler(OnCarEngineEvent2);
 
 c1.RegisterWithCarEngine(handler2);
 
+
+// register a simple methof for c2 using 
+// group conversion syntax
+c2.RegisterWithCarEngine(OnCarEngineEvent);
+
 // speed up (this will trigger events)
 Console.WriteLine("***** Speeding up *****");
 for (int i = 0; i < 9; i++)
 {
     c1.Accelerate(20);
 }
+
+// unregister c2 method
+c2.UnRegisterWithCarEngine(OnCarEngineEvent);
 
 // remove the upper case handler
 c1.UnRegisterWithCarEngine(handler2);
