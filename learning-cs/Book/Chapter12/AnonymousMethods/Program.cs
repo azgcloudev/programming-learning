b4b4e5e714993377587f332e2cd7ab0057d10914
Aldair Zamora <aldair.zamora@outlook.com>
2024-08-04 13:54:17 -0600

    anonymous methods

--- learning-cs/Book/Chapter12/AnonymousMethods/Program.cs
+++ learning-cs/Book/Chapter12/AnonymousMethods/Program.cs
@@ -1,17 +1,20 @@
 ﻿using AnonymousMethods;
 
 Console.WriteLine("***** Anonymous Methods *****\n");
+int aboutToBlowCounter = 0;
 
 var c1 = new Car("Slugbug", 100, 10);
 
 // register event handlers as anonymous methods
 c1.AboutToBlow += delegate
 {
+    aboutToBlowCounter++;
     Console.WriteLine("Eek! Going too fast!");
 };
 
 c1.AboutToBlow += delegate(object sender, CarEventsArgs e)
 {
+    aboutToBlowCounter++;
     Console.WriteLine("Message from Car: {0}", e.msg);
 };
 
@@ -25,4 +28,6 @@ for (int i = 0; i < 6; i++)
 {
     c1.Accelerate(20);
 }
+
+Console.WriteLine("AboutToBlow even was fired {0} times", aboutToBlowCounter);
 Console.ReadLine();
