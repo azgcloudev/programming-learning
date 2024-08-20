851492a0b5c3d516d51603b95889787dbfb204ff
Aldair Zamora <aldair.zamora@outlook.com>
2024-08-19 22:50:49 -0600

    linq as return

---
+++ learning-cs/Book/Chapter13/LinqRetValues/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("***** LINQ Return Values *****\n");
+
+IEnumerable<string> subset = GetStringSubset();
+
+foreach (string item in subset)
+{
+    Console.WriteLine(item);
+}
+
+// inmediate execution
+Console.WriteLine("\n***** Immediate execution *****");
+foreach (var item in GetStringSubsetAsArray())
+{
+    Console.WriteLine(item);
+}
+// =====================================
+
+static IEnumerable<string> GetStringSubset()
+{
+    string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple" };
+    // note that subset is IEnumerable<string> compatible
+    IEnumerable<string> theRedColors = from c in colors where c.Contains("Red") select c;
+    return theRedColors;
+}
+
+static string[] GetStringSubsetAsArray()
+{
+    string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple" };
+
+    var theRedColors = from c in colors where c.Contains("Red") select c;
+    
+    // use inmediate execution
+    return theRedColors.ToArray();
+}
