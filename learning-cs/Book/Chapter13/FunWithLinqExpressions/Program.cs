7ff341bf8f9d2fcfcc58400af2c903225eb6fc95
Aldair Zamora <aldair.zamora@outlook.com>
2024-09-01 23:36:26 -0600

    linq paging

--- learning-cs/Book/Chapter13/FunWithLinqExpressions/Program.cs
+++ learning-cs/Book/Chapter13/FunWithLinqExpressions/Program.cs
@@ -51,6 +51,9 @@ class Program
         Console.WriteLine();
         PagingWithLINQ(itemsInStock);
 
+        Console.WriteLine();
+        PagingWithRanges(itemsInStock);
+
         Console.ReadLine();
     }
 
@@ -106,6 +109,28 @@ class Program
         IEnumerable<ProductInfo> list = (from p in products select p).Take(3);
         OutputResults("The first 3", list);
 
+        // take while a condition is met
+        IEnumerable<ProductInfo> list2 = (from p in products select p).TakeWhile(x => x.NumberInStock > 20);
+        OutputResults("All while number in stock > 20", list2);
+
+        // takes the last specified number of records
+        IEnumerable<ProductInfo> list3 = (from p in products select p).TakeLast(2);
+        OutputResults("last 2 records", list3);
+
+        // skip the first three records
+        IEnumerable<ProductInfo> list4 = (from p in products select p).Skip(3);
+        OutputResults("Skips the first 3", list4);
+
+        // skip while a condition, same as takewhile, when the condition returns false
+        // it stops and do not continue with more record, is better to sort (orderby) before
+        // calling the method
+        IEnumerable<ProductInfo> list5 = (from p in products select p).SkipWhile(x => x.NumberInStock > 20);
+        OutputResults("Skip while number in stock > 20", list5);
+
+        // takes all except the last count 
+        IEnumerable<ProductInfo> list6 = (from p in products select p).SkipLast(2);
+        OutputResults("All but the last 2", list6);
+
         static void OutputResults(string message, IEnumerable<ProductInfo> products)
         {
             Console.WriteLine(message);
@@ -116,4 +141,38 @@ class Program
         }
     }
 
+    /// <summary>
+    /// Paging with ranges support with .net > 6 and c# > 10.
+    /// </summary>
+    static void PagingWithRanges(ProductInfo[] products)
+    {
+        Console.WriteLine("***** Paging Operations (with ranges) ******");
+
+        IEnumerable<ProductInfo> list = (from p in products select p).Take(..3);
+        OutputResults("The first 3", list);
+
+        list = (from p in products select p).Take(3..);
+        OutputResults("Skipping the first 3", list);
+
+        list = (from p in products select p).Take(^2..);
+        OutputResults("The last 2", list);
+
+        list = (from p in products select p).Take(3..2);
+        OutputResults("Skip 3 and take 2", list);
+
+        list = (from p in products select p).Take(..^2);
+        OutputResults("All except the last 2", list);
+
+        static void OutputResults(string message, IEnumerable<ProductInfo> products)
+        {
+            Console.WriteLine(message);
+
+            foreach(ProductInfo p in products)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+
+   
 }
