0a9b4690a44c596ce110078c4fe4efba4b6d2a6e
Aldair Zamora <aldair.zamora@outlook.com>
2024-09-12 10:15:08 -0600

    Complete chapter 13

---
+++ learning-cs/Book/Chapter13/LinqUsingEnumerable/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using LinqUsingEnumerable;
+
+QueryStringWithOperators();
+
+Console.WriteLine();
+QueryStringsWithEnumerableAndLambdas();
+
+Console.WriteLine();
+QueryStringsWithAnonymousMethods();
+
+Console.WriteLine();
+VeryComplexQueryExpression.QueryStringsWithRawDelegates();
+
+
+Console.ReadLine();
+
+static void QueryStringWithOperators()
+{
+    Console.WriteLine(">>>>> Using query expressions <<<<<");
+
+    string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "SystemShock 2" };
+
+    var result = from game in currentVideoGames where game.Contains(" ") orderby game select game;
+
+    foreach (var game in result)
+    {
+        Console.WriteLine(game);
+    }
+}
+
+static void QueryStringsWithEnumerableAndLambdas()
+{
+    Console.WriteLine(">>>>> Using Enumerable / Lambda Expressions <<<<<");
+
+    string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "SystemShock 2" };
+
+    // build the query using extension methods granted by Array Enumerable type.
+    var result = currentVideoGames.Where(game => game.Contains(" "))
+        .OrderBy(game => game)
+        .Select(game => game);
+
+    foreach (var item in result)            
+    {
+        Console.WriteLine(item);
+    }
+}
+
+static void QueryStringsWithAnonymousMethods()
+{
+    Console.WriteLine(">>>>> Using Anonymous Methods <<<<<");
+
+    string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "SystemShock 2"};
+
+    // build the Func<> delegates using anonymous methods
+    Func<string, bool> searchFilter = delegate(string game)
+    {
+        return game.Contains(" ");
+    };
+    Func<string, string> itemToProcess = delegate(string s)
+    {
+        return s;
+    };
+
+    // pass the delegates into the methods
+    var subset = currentVideoGames.Where(searchFilter).OrderBy(itemToProcess).Select(itemToProcess);
+
+    foreach (var item in subset)
+    {
+        Console.WriteLine(item);
+    }
+}
---
+++ learning-cs/Book/Chapter13/LinqUsingEnumerable/VeryComplexQueryExpression.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUsingEnumerable
+{
+    internal class VeryComplexQueryExpression
+    {
+        public static void QueryStringsWithRawDelegates()
+        {
+            Console.WriteLine(">>>>> Using Raw Delegates <<<<<");
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "SystemShock 2" };
+
+            // build the func<> delegates
+            Func<string, bool> searchFilter = new Func<string, bool>(Filter);
+            Func<string, string> itemToProcess = new Func<string, string>(ProcessItem);
+
+            var result = currentVideoGames.Where(searchFilter)
+                .OrderBy(itemToProcess)
+                .Select(itemToProcess);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        // delegate targets
+        public static bool Filter(string s)
+        {
+            return s.Contains(" ");
+        }
+
+        public static string ProcessItem(string s)
+        {
+            return s;
+        }
+    }
+}
