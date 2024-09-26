6c348d8209d122ad6f1aa18df91d72ce5ceae4fc
Aldair Zamora <aldair.zamora@outlook.com>
2024-09-26 11:14:28 -0600

    start chapter 14

---
+++ learning-cs/Book/Chapter14/ProcessManipulator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+static void ListAllRunningProcesses()
+{
+    // Get all the processes on the local machine, ordered by PID
+    var runningProcs = from proc in Process.GetProcesses(".")
+        orderby proc.Id
+        select proc;
+
+
+}
