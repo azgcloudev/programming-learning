fa1a8e00b10fa8e93563d656b839aa454ae1951d
Aldair Zamora <aldair.zamora@outlook.com>
2024-12-09 00:24:07 -0600

    current thread execution

--- learning-cs/Book/Chapter15/ThreadsP/Program.cs
+++ learning-cs/Book/Chapter15/ThreadsP/Program.cs
@@ -16,3 +16,9 @@ static void ExtractAppDomainHostingThread()
     AppDomain ad = Thread.GetDomain();
     Console.WriteLine("Current thread AppDomain: {0}", ad.FriendlyName);
 }
+
+// obtein the execution context of the current thread
+static void ExtractCurrentThreadExecutionContext()
+{
+    ExecutionContext ctx = Thread.CurrentThread.ExecutionContext;
+}
