71f7b47b5fa1272be7725d9eecf85b27623dc082
Aldair Zamora <aldair.zamora@outlook.com>
2024-12-10 11:09:18 -0600

    working with concurrency issues

---
+++ learning-cs/Book/Chapter15/MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadedPrinting;
+
+public class Printer
+{
+    public void PrintNumbers()
+    {
+        // display the thread info
+        Console.WriteLine($"-> {Thread.CurrentThread.Name} is executing PrintNumbers()");
+        
+        // Print the numbers
+        for (int i = 0; i < 10; i++)
+        {
+            Random r = new Random();
+            Thread.Sleep(1000 * r.Next(5));
+            Console.Write($"{i}, ");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ learning-cs/Book/Chapter15/MultiThreadedPrinting/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MultiThreadedPrinting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*****Synchronizing Threads *****\n");
+        
+        Printer p = new Printer();
+
+        Thread[] threads = new Thread[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+            {
+                Name = $"Thread #: {i}"
+            };
+        }
+        
+        // start each thread
+        foreach (Thread t in threads)   
+        {
+            t.Start();
+        }
+
+        Console.ReadLine();
+    }
+}
