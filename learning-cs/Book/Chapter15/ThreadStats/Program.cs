9f85568eb53133540c9574e42eebb669385982ae
Aldair Zamora <aldair.zamora@outlook.com>
2024-12-09 23:07:30 -0600

    complete ThreadStats

--- learning-cs/Book/Chapter15/ThreadStats/Program.cs
+++ learning-cs/Book/Chapter15/ThreadStats/Program.cs
@@ -11,3 +11,9 @@ primaryThread.Name = "ThePrimaryThread";
 Console.WriteLine("ID of current thread: {0}", primaryThread.ManagedThreadId);
 Console.WriteLine("Thread name: {0}", primaryThread.Name);
 Console.WriteLine("Has thread started?: {0}", primaryThread.IsAlive);
+// update priority from ENUM ThreadPriority
+primaryThread.Priority = ThreadPriority.AboveNormal;
+Console.WriteLine("Priority Level: {0}", primaryThread.Priority);
+Console.WriteLine("Thread State: {0}", primaryThread.ThreadState);
+Console.ReadKey();
+
