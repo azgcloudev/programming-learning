327c5d275251df03504856428b07c9a6668083c3
Aldair Zamora <aldair.zamora@outlook.com>
2024-12-10 10:52:46 -0600

    complete background thread example

---
+++ learning-cs/Book/Chapter15/BackgroundThreads/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BackgroundThreads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Background Threads *****\n");
+
+            Printer p = new Printer();
+
+            Thread backgroundThread = new Thread(new ThreadStart(p.PrintNumbers));
+
+            // set the thread as background
+            backgroundThread.IsBackground = true;
+            backgroundThread.Start();
+        }
+    }
+}
