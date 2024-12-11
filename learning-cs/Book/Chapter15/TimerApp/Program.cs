3752569bb985b10c7e5b20395339e080cab77d81
Aldair Zamora <aldair.zamora@outlook.com>
2024-12-11 00:01:01 -0600

    TimerCallback example

---
+++ learning-cs/Book/Chapter15/TimerApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TimerApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Working with Timer type *****\n");
+
+        // create delegate
+        TimerCallback timerCB = new TimerCallback(PrintTime);
+
+        // timer settings
+        Timer _ = new Timer(
+            timerCB, // delegate
+            "<TIME> =>", // info to pass in the called method
+            0, // time to wait before starting
+            1000 // interval in miliseconds
+        );
+    
+        Console.WriteLine("Enter key to terminate...");
+        Console.ReadLine();
+    }
+
+    static void PrintTime(object? state)
+    {
+        Console.WriteLine($"{state.ToString()} Current time is {DateTime.Now.ToLongTimeString()}");
+    }
+}
