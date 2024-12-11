14f7e87cc1fab2306a12d1efe0135b26ed945414
Aldair Zamora <aldair@Aldairs-Air.lan>
2024-12-10 22:58:45 -0600

    thread lock with lock keyword

--- learning-cs/Book/Chapter15/MultiThreadedPrinting/Printer.cs
+++ learning-cs/Book/Chapter15/MultiThreadedPrinting/Printer.cs
@@ -5,19 +5,25 @@ namespace MultiThreadedPrinting;
 
 public class Printer
 {
+    private object threadLock = new object();
+
     public void PrintNumbers()
     {
-        // display the thread info
-        Console.WriteLine($"-> {Thread.CurrentThread.Name} is executing PrintNumbers()");
-        
-        // Print the numbers
-        for (int i = 0; i < 10; i++)
+        // lock thread
+        lock (threadLock)
         {
-            Random r = new Random();
-            Thread.Sleep(1000 * r.Next(5));
-            Console.Write($"{i}, ");
-        }
+            // display the thread info
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} is executing PrintNumbers()");
 
-        Console.WriteLine();
+            // Print the numbers
+            for (int i = 0; i < 10; i++)
+            {
+                Random r = new Random();
+                Thread.Sleep(1000 * r.Next(5));
+                Console.Write($"{i}, ");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
