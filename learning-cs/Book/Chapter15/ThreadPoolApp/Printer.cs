ab5a9fb0006f2fbc3d6061c9d389985ca5dd2e67
Aldair Zamora <aldair.zamora@outlook.com>
2024-12-11 00:23:35 -0600

    Threadpool examples

---
+++ learning-cs/Book/Chapter15/ThreadPoolApp/Printer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolApp;
+
+public class Printer
+{
+    private object threadLock = new object();
+
+    public void PrintNumbers()
+    {
+        Thread thread = Thread.CurrentThread;
+        thread.Name = "Printing thread";
+        
+        // locking thread using System.Threading.Monitor
+        Monitor.Enter(threadLock);
+        try
+        {
+            // display the thread info
+            Console.WriteLine($"-> {thread.Name}:id={thread.ManagedThreadId} is executing PrintNumbers()");
+
+            // Print the numbers
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(50);
+                Console.Write($"{i}, ");
+            }
+
+            Console.WriteLine();
+        }
+        finally
+        {
+            Monitor.Exit(threadLock);
+        }
+    }
+}
---
+++ learning-cs/Book/Chapter15/ThreadPoolApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using ThreadPoolApp;
+
+Console.WriteLine("***** Fun with the .NET Core Runtime Thread Pool *****\n");
+
+Console.WriteLine("Current main threaed running with id {0}", Environment.CurrentManagedThreadId);
+
+Printer p = new Printer();
+
+WaitCallback workitem = new(PrintTheNumbers);
+
+// queue the method 10 times
+for (int i = 0; i < 10; i++)
+{
+    ThreadPool.QueueUserWorkItem(workitem, p);
+}
+
+Console.WriteLine("All tasks queued");
+Console.ReadLine();
+
+static void PrintTheNumbers(object? state)
+{
+    Printer task = (Printer)state;
+    task.PrintNumbers();
+}
