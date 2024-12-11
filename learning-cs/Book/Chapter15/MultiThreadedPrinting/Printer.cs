0d24591b1669c598a35841e4ed345f9cdd1d33fd
Aldair Zamora <aldair@Aldairs-Air.lan>
2024-12-10 23:40:09 -0600

    complete lock System.Threading.Monitor

--- learning-cs/Book/Chapter15/MultiThreadedPrinting/Printer.cs
+++ learning-cs/Book/Chapter15/MultiThreadedPrinting/Printer.cs
@@ -9,6 +9,8 @@ public class Printer
 
     public void PrintNumbers()
     {
+        /***** lock using lock keyword
+         *
         // lock thread
         lock (threadLock)
         {
@@ -24,6 +26,29 @@ public class Printer
             }
 
             Console.WriteLine();
+        }
+        */
+
+        // locking thread using System.Threading.Monitor
+        Monitor.Enter(threadLock);
+        try
+        {
+            // display the thread info
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} is executing PrintNumbers()");
+
+            // Print the numbers
+            for (int i = 0; i < 10; i++)
+            {
+                Random r = new Random();
+                Thread.Sleep(1000 * r.Next(5));
+                Console.Write($"{i}, ");
+            }
+
+            Console.WriteLine();
+        }
+        finally
+        {
+            Monitor.Exit(threadLock);
         }
     }
 }
