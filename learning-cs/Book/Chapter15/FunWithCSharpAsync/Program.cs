5f5b2f52ebb6836a1b2e4a579d31f60b40576949
Aldair Zamora <aldair.zamora@outlook.com>
2024-12-28 21:21:20 -0600

    review of Task.WhenAny()

--- learning-cs/Book/Chapter15/FunWithCSharpAsync/Program.cs
+++ learning-cs/Book/Chapter15/FunWithCSharpAsync/Program.cs
@@ -1,23 +1,26 @@
 ﻿Console.WriteLine("-> Having fun with ASYNC");
-Console.WriteLine("Starting thread lock");
-Console.WriteLine(DoWork());
+//Console.WriteLine("Starting thread lock");
+//Console.WriteLine(DoWork());
 
-Console.WriteLine("Start async");
-string message = await DoWorkAsync();
-Console.WriteLine(message);
-try
-{
+//Console.WriteLine("Start async");
+// string message = await DoWorkAsync();
+// Console.WriteLine(message);
+// try
+// {
     // async methods should be await in order to catch an exception
-    await MethodReturningVoidAsync();
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.Message);
-}
+    // await MethodReturningVoidAsync();
+// }
+// catch (Exception e)
+// {
+    // Console.WriteLine(e.Message);
+// }
 
-Console.WriteLine("Completed");
+// await MultipleAwaits();
+// await MultipleAwaitAsync();
 
-Console.ReadLine();
+await MultipleAwaitWhenAnyAsync();
+
+// Console.WriteLine("Completed");
 
 static string DoWork()
 {
@@ -44,4 +47,71 @@ static async Task MethodReturningVoidAsync() // void async methods should be Tas
         throw new Exception("Method returning void");
     });
     Console.WriteLine("MethodReturningVoidAsync is completed");
+}
+
+
+//// multiple awaits in an async method
+static async Task MultipleAwaits()
+{
+    // this task are processed in order of sleep from low to max
+    await Task.Run(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("First task completed");
+    });
+    await Task.Run(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Second task completed");
+    });
+    await Task.Run(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Third task completed");
+    });
+}
+
+// multiple operations in a single await
+static async Task MultipleAwaitAsync()
+{
+    await Task.WhenAll(
+        Task.Run(() =>
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("1st task completed");
+        }),
+        Task.Run(() =>
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("2nd task completed");
+        }),
+        Task.Run(() =>
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("3rd task completed");
+        }));
+}
+
+//await returns the first Task completed
+static async Task MultipleAwaitWhenAnyAsync()
+{
+    await Task.WhenAny(Task.Run(() =>
+    {
+        var r = new Random();
+        int n = r.Next(0, 4000);
+        Thread.Sleep(n);
+        Console.WriteLine("1st task completed");
+    }), Task.Run(() =>
+    {
+        var r = new Random();
+        int n = r.Next(0, 4000);
+        Thread.Sleep(n);
+        Console.WriteLine("2nd task completed");
+    }), Task.Run(() =>
+    {
+        var r = new Random();
+        int n = r.Next(0, 4000);
+        Thread.Sleep(n);
+        Console.WriteLine("3rd task completed");
+    }));
 }
