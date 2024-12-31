7edfd92d753871443d70994d5d3c8e7e5b3a5928
Aldair Zamora <aldair.zamora@outlook.com>
2024-12-31 10:26:17 -0600

    Review of configuration files in JSON "appsettings.json"

---
+++ learning-cs/Book/Chapter16/FunWithConfiguration/Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new  ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json",true,true)
+    .Build();
+
+Console.WriteLine($"My cars name is {config["CarName"]}");
