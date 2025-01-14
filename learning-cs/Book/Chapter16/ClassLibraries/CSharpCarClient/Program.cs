b6aa693324486964294b3bbef154fb128d7d09fe
Aldair Zamora <aldair.zamora@outlook.com>
2025-01-14 09:53:12 -0600

    Class libraries and project references

---
+++ learning-cs/Book/Chapter16/ClassLibraries/CSharpCarClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using CarLibrary;
+
+Console.WriteLine("***** C# CarLibrary Client App *****");
+
+// make sports car
+SportsCar viper = new SportsCar("Viper", 240,50);
+viper.TurboBoost();
+
+// make mini van
+MiniVan mv = new MiniVan();
+mv.TurboBoost();
+
+Console.WriteLine("Done. Press any key to terminate");
+Console.ReadKey();
