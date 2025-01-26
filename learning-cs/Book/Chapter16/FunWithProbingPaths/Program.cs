10c02f429eef19b104f4b2f8a7e5ddad105e93b5
Aldair Zamora <aldair.zamora@outlook.com>
2025-01-26 16:48:17 -0600

    complete chapter 16

---
+++ learning-cs/Book/Chapter16/FunWithProbingPaths/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("*** Fun with Probing Paths ***");
+
+Console.WriteLine($"TRUSTED_PLATFORM_ASSEMBLIES: ");
+var list = AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES")?.ToString()?.Split(";");
+if (list != null)
+{
+    Console.WriteLine("Count of directories: {0}", list.Length);
+    foreach (var dir in list)
+    {
+        Console.WriteLine(dir);
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"PLATFORM_RESOURCE_ROOTS: {AppContext.GetData("PLATFORM_RESOURCE_ROOTS")}");
+
+Console.WriteLine();
+Console.WriteLine($"NATIVE-DLL-SEARCH-DIRECTORIES {AppContext.GetData("NATIVE_DLL_SEARCH_DIRECTORIES")}");
+
+Console.WriteLine();
+Console.WriteLine($"APP_PATHS {AppContext.GetData("APP_PATHS")}");
+
+Console.WriteLine();
+Console.WriteLine($"APP_NI_PATHS {AppContext.GetData("APP_NI_PATHS")}");
