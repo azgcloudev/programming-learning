67881b8a3467f162e312306b767f2d2d17924740
Aldair Zamora <aldair.zamora@outlook.com>
2025-02-12 20:59:33 -0600

    intro to classes

---
+++ learning-cs/Book/Chapter05/SimpleClassExample/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    internal class Car
+    {
+        // the state of the Car
+        public string petName;
+        public int currSpeed;
+
+        // default constructor
+        public Car()
+        {
+            petName = "Chuck";
+            currSpeed = 10;
+        }
+
+        // constructor to define name, with default type value
+        public Car(string pn)
+        {
+            petName = pn;
+        }
+
+        // functionality of the Car
+        public void PrintState()
+                => Console.WriteLine("{0} is going {1} MPH", petName, currSpeed);
+
+        public void SpeedUp(int delta)
+            => currSpeed += delta;
+    }
+}
---
+++ learning-cs/Book/Chapter05/SimpleClassExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using SimpleClassExample;
+
+Console.WriteLine("***** Fun with Class types *****\n");
+
+// allo cate and  configura a Car
+Car myCar = new Car();
+myCar.petName = "Henry";
+myCar.currSpeed = 10;
+
+// speed up the car a few times and print out the 
+// new state
+for (int i = 0; i <= 10; i++)
+{
+    myCar.SpeedUp(5);
+    myCar.PrintState();
+}
+
+Console.ReadLine();
