1eb46408c177f0d6dd512a9767044273df99677c
Aldair Zamora <aldair.zamora@outlook.com>
2025-04-29 00:30:02 -0600

    estudio

--- learning-cs/Book/Chapter05/ObjectInitializers/Point.cs
+++ learning-cs/Book/Chapter05/ObjectInitializers/Point.cs
@@ -4,19 +4,27 @@ public class Point
 {
     public int X { get; set; }
     public int Y { get; set; }
+    public PointColorEnum Color { get; set; }
 
     public Point(int xVal, int yVal)
     {
         X = xVal;
         Y = yVal;
+        Color = PointColorEnum.Gold;
     }
 
-    public Point()
+    public Point(PointColorEnum ptColor)
+    {
+        Color = ptColor;
+    }
+
+    public Point() : this(PointColorEnum.BlueRed)
     {
     }
 
     public void DisplayStats()
     {
         Console.WriteLine("[{0}, {1}]", X, Y);
+        Console.WriteLine("Point is {0}", Color);
     }
 }
