172dcd31aba47b5c177444dc818b882a9a7169b7
Aldair Zamora <aldair.zamora@outlook.com>
2025-08-06 09:27:46 -0600

    study

---
+++ learning-cs/BookMarc/Chapter04/TopLevelFunctions/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+WriteLine("Top-level function examples");
+WhatsMyNamespace();
+
+void WhatsMyNamespace()
+{
+    WriteLine("Namespace of program class: {0}", arg0: typeof(Program).Namespace ?? "null");
+}
